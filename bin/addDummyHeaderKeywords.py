11a8de2cfc999f1696480bfca68f871a4ea5bd60
Andreas Ritter <azuri.ritter@gmail.com>
2015-12-15 10:40:25 +0100

    ADDED EXAMPLES HOW TO RUN INGEST, LIST REPOSITORY CONTENTS, GET IMAGE FROM BUTLER

---
+++ bin/addDummyHeaderKeywords.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+from pfs.drp.stella.addDummyHeaderKeywordsCmdLineTask import AddDummyHeaderKeywordsCmdLineTask
+AddDummyHeaderKeywordsCmdLineTask.parseAndRun()
+#drpStella.addDummyHeaderKeywordsCmdLineTask.AddDummyHeaderKeywordsCmdLineTask.parseAndRun()
---
+++ examples/getImageFromButler.py
@@ -0,0 +1,7 @@
+import argparse
+import numpy
+import lsst.daf.persistence as dafPersist
+butler = dafPersist.Butler('/tigress/HSC/PFS/2015-11-20/')
+dataId = {'visit': '5441', 'ccd': 2}
+im = butler.get('raw', dataId)
+
