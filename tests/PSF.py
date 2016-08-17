3c33555ac14a725fc652c93cee5db1d222aea76c
Andreas Ritter <azuri.ritter@gmail.com>
2016-08-16 22:10:44 -0400

    Update data dir, silence warnings

--- tests/PSF.py
+++ tests/PSF.py
@@ -32,10 +32,10 @@ class PSFTestCase(tests.TestCase):
     def setUp(self):
         drpStellaDataDir = lsst.utils.getPackageDir("drp_stella_data")
         flatfile = os.path.join(drpStellaDataDir,"tests/data/PFS/CALIB/FLAT/r/flat/pfsFlat-2016-01-12-0-2r.fits")
-        self.flat = afwImage.ExposureF(flatfile)
+        self.flat = afwImage.makeExposure(afwImage.makeMaskedImage(afwImage.ImageF(flatfile)))
 
         arcfile = os.path.join(drpStellaDataDir,"tests/data/PFS/postISRCCD/2016-01-12/v0000004/PFFAr2.fits")
-        self.arc = afwImage.ExposureF(arcfile)
+        self.arc = afwImage.makeExposure(afwImage.makeMaskedImage(afwImage.ImageF(arcfile)))
         
         self.ftffc = drpStella.FiberTraceFunctionFindingControl()
         self.ftffc.fiberTraceFunctionControl.order = 4
