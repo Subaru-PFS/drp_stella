80837c79fb40feaee452b36713b70da006def9a1
Andreas Ritter <azuri.ritter@gmail.com>
2016-10-07 00:57:07 -0400

    Read FiberTrace from disk instead of tracing the apertures

--- python/pfs/drp/stella/reduceArcTask.py
+++ python/pfs/drp/stella/reduceArcTask.py
@@ -2,10 +2,9 @@
 import os
 import sys
 
-import pfs.drp.stella.findAndTraceAperturesTask as fataTask
-import pfs.drp.stella.createFlatFiberTraceProfileTask as cfftpTask
 import pfs.drp.stella.extractSpectraTask as esTask
 import pfs.drp.stella as drpStella
+from pfs.drp.stella.utils import makeFiberTraceSet
 from lsst.utils import getPackageDir
 from lsst.pex.config import Config, Field
 from lsst.pipe.base import Struct, TaskRunner, ArgumentParser, CmdLineTask
@@ -79,192 +78,172 @@ class ReduceArcTask(CmdLineTask):
         self.log.info('wLenFile = %s' % wLenFile)
         self.log.info('lineList = %s' % lineList)
 
-        arcRef = expRefList[0]
-        self.log.info('arcRef.dataId = %s' % arcRef.dataId)
-        self.log.info('arcRef = %s' % arcRef)
-        self.log.info('type(arcRef) = %s' % type(arcRef))
+        for arcRef in expRefList:
+            self.log.info('arcRef.dataId = %s' % arcRef.dataId)
+            self.log.info('arcRef = %s' % arcRef)
+            self.log.info('type(arcRef) = %s' % type(arcRef))
 
-	try:
-            flatExposure = arcRef.get('flat', immediate=immediate)
-        except Exception, e:
-            raise RuntimeError("Unable to retrieve flat for %s: %s" % (arcRef.dataId, e))
-        
-        arcExp = arcRef.get("postISRCCD", immediate=True)
-        self.log.info('arcExp = %s' % arcExp)
-        self.log.info('type(arcExp) = %s' % type(arcExp))
-
-        """ find and trace fiber traces """
-        print 'tracing flat fiber traces'
-        myFindTask = fataTask.FindAndTraceAperturesTask()
-        flatFiberTraceSet = myFindTask.run(flatExposure)
-        print flatFiberTraceSet.size(),' traces found'
-
-        """ calculate spatial profiles """
-        print 'calculating spatial profiles'
-        myProfileTask = cfftpTask.CreateFlatFiberTraceProfileTask()
-        myProfileTask.run(flatFiberTraceSet)
-        
-        """ optimally extract arc spectra """
-        print 'extracting arc spectra'
-
-        """ read wavelength file """
-        hdulist = pyfits.open(wLenFile)
-        tbdata = hdulist[1].data
-        traceIdsTemp = np.ndarray(shape=(len(tbdata)), dtype='int')
-        xCenters = np.ndarray(shape=(len(tbdata)), dtype='float32')
-        yCenters = np.ndarray(shape=(len(tbdata)), dtype='float32')
-        wavelengths = np.ndarray(shape=(len(tbdata)), dtype='float32')
-        traceIdsTemp[:] = tbdata[:]['fiberNum']
-        traceIds = traceIdsTemp.astype('int32')
-        wavelengths[:] = tbdata[:]['pixelWave']
-        xCenters[:] = tbdata[:]['xc']
-        yCenters[:] = tbdata[:]['yc']
-        
-        traceIdsUnique = np.unique(traceIds)
-
-        """ assign trace number to flatFiberTraceSet """
-        success = drpStella.assignITrace( flatFiberTraceSet, traceIds, xCenters, yCenters )
-        iTraces = np.ndarray(shape=flatFiberTraceSet.size(), dtype='intp')
-        for i in range( flatFiberTraceSet.size() ):
-            iTraces[i] = flatFiberTraceSet.getFiberTrace(i).getITrace()
-
-        if success == False:
-            print 'assignITrace FAILED'
-
-        myExtractTask = esTask.ExtractSpectraTask()
-        aperturesToExtract = [-1]
-        spectrumSetFromProfile = myExtractTask.run(arcExp, flatFiberTraceSet, aperturesToExtract)
-            
-        for i in range(spectrumSetFromProfile.size()):
-            # THIS DOESN'T WORK, again a problem with changing getSpectrum()
-            spectrumSetFromProfile.getSpectrum(i).setITrace(flatFiberTraceSet.getFiberTrace(i).getITrace())
-#            print 'spectrumSetFromProfile[',i,'].iTrace = ',spectrumSetFromProfile.getSpectrum(i).getITrace()
-
-#        fig = plt.figure()
-#        ax = fig.add_subplot(1, 1, 1)
-#        for i in range(spectrumSetFromProfile.size()):
-#            ax.plot(spectrumSetFromProfile.getSpectrum(i).getSpectrum(),'-+')
-#            plt.xlim(1450,1600)
-#            plt.ylim(0,8000)
-#        plt.show()
-#        plt.close(fig)
-#        fig.clf()
-        
-        """ read line list """
-        hdulist = pyfits.open(lineList)
-        tbdata = hdulist[1].data
-        lineListArr = np.ndarray(shape=(len(tbdata),2), dtype='float32')
-        lineListArr[:,0] = tbdata.field(0)
-        lineListArr[:,1] = tbdata.field(1)
-        
-        dispCorControl = drpStella.DispCorControl()
-        dispCorControl.fittingFunction = self.config.function
-        dispCorControl.order = self.config.order
-        dispCorControl.searchRadius = self.config.searchRadius
-        dispCorControl.fwhm = self.config.fwhm
-        self.log.info('dispCorControl.fittingFunction = %s' % dispCorControl.fittingFunction)
-        self.log.info('dispCorControl.order = %d' % dispCorControl.order)
-        self.log.info('dispCorControl.searchRadius = %d' % dispCorControl.searchRadius)
-        self.log.info('dispCorControl.fwhm = %g' % dispCorControl.fwhm)
-
-        for i in range(spectrumSetFromProfile.size()):
-            spec = spectrumSetFromProfile.getSpectrum(i)
-            spec.setITrace(iTraces[i])
-            self.log.info('flatFiberTraceSet.getFiberTrace(%d).getITrace() = %d, spec.getITrace() = %d' %(i,flatFiberTraceSet.getFiberTrace(i).getITrace(), spec.getITrace()))
-            specSpec = spec.getSpectrum()
-            self.log.info('yCenters.shape = %d' % yCenters.shape)
-            self.log.info('specSpec.shape = %d' % specSpec.shape)
-            self.log.info('lineListArr.shape = [%d,%d]' % (lineListArr.shape[0], lineListArr.shape[1]))
-            self.log.info('type(specSpec) = %s: <%s>' % (type(specSpec),type(specSpec[0])))
-            self.log.info('type(lineListArr) = %s: <%s>' % (type(lineListArr),type(lineListArr[0][0])))
-            self.log.info('type(spec) = %s: <%s>: <%s>' % (type(spec),type(spec.getSpectrum()),type(spec.getSpectrum()[0])))
-            
-            traceId = spec.getITrace()
-#            print 'traceId = ',traceId
-            wLenTemp = np.ndarray( shape = traceIds.shape[0] / np.unique(traceIds).shape[0], dtype='float32' )
-            k = 0
-            l = -1
-            for j in range(traceIds.shape[0]):
-                if traceIds[j] != l:
-                    l = traceIds[j]
-                if traceIds[j] == traceIdsUnique[traceId]:
-                    wLenTemp[k] = wavelengths[j]
-                    k = k+1
-                  
-            """cut off both ends of wavelengths where is no signal"""
-            xCenter = flatFiberTraceSet.getFiberTrace(i).getFiberTraceFunction().xCenter
-            yCenter = flatFiberTraceSet.getFiberTrace(i).getFiberTraceFunction().yCenter
-            yLow = flatFiberTraceSet.getFiberTrace(i).getFiberTraceFunction().yLow
-            yHigh = flatFiberTraceSet.getFiberTrace(i).getFiberTraceFunction().yHigh
-            yMin = yCenter + yLow
-            yMax = yCenter + yHigh
-            self.log.info('fiberTrace %d: xCenter = %d' % (i, xCenter))
-            self.log.info('fiberTrace %d: yCenter = %d' % (i, yCenter))
-            self.log.info('fiberTrace %d: yLow = %d' % (i, yLow))
-            self.log.info('fiberTrace %d: yHigh = %d' % (i, yHigh))
-            self.log.info('fiberTrace %d: yMin = %d' % (i, yMin))
-            self.log.info('fiberTrace %d: yMax = %d' % (i, yMax))
-            wLen = wLenTemp[ yMin + self.config.nRowsPrescan : yMax + self.config.nRowsPrescan + 1]
-            wLenArr = np.ndarray(shape=wLen.shape, dtype='float32')
-            for j in range(wLen.shape[0]):
-                wLenArr[j] = wLen[j]
-            wLenLines = lineListArr[:,0]
-            wLenLinesArr = np.ndarray(shape=wLenLines.shape, dtype='float32')
-            for j in range(wLenLines.shape[0]):
-                wLenLinesArr[j] = wLenLines[j]
-            lineListPix = drpStella.createLineList(wLenArr, wLenLinesArr)
             try:
-                spec.identifyF(lineListPix, dispCorControl, 8)
-            except:
-                e = sys.exc_info()[1]
-                message = str.split(e.message, "\n")
-                for k in range(len(message)):
-                    print "element",k,": <",message[k],">"
-            print "FiberTrace ",i,": spec.getDispCoeffs() = ",spec.getDispCoeffs()
-            print "FiberTrace ",i,": spec.getDispRms() = ",spec.getDispRms()
-            if spectrumSetFromProfile.setSpectrum(i, spec ):
-                print 'setSpectrum for spectrumSetFromProfile[',i,'] done'
-            else:
-                print 'setSpectrum for spectrumSetFromProfile[',i,'] failed'
+                fiberTrace = arcRef.get('fiberTrace', immediate=True)
+            except Exception, e:
+                raise RuntimeError("Unable to retrieve fiberTrace for %s: %s" % (arcRef.dataId, e))
 
-        if False:#disable plotting the data
-            xPixMinMax = np.ndarray(2, dtype='float32')
-            xPixMinMax[0] = 1000.
-            xPixMinMax[1] = 1600.
-            fig = plt.figure()
-            ax = fig.add_subplot(1, 1, 1)
-            for i in range(spectrumSetFromProfile.size()):
-                ax.plot(spectrumSetFromProfile.getSpectrum(i).getSpectrum(),'-+')
-                plt.xlim(xPixMinMax[0],xPixMinMax[1])
-                plt.ylim(0,25000)
-            plt.xlabel('Pixel')
-            plt.ylabel('Flux [ADUs]')
-            plt.show()
-            plt.close(fig)
-            fig.clf()
-            
-            xMinMax = drpStella.poly(xPixMinMax, spec.getDispCoeffs())
-            fig = plt.figure()
-            ax = fig.add_subplot(1, 1, 1)
-            for i in range(spectrumSetFromProfile.size()):
-                ax.plot(spectrumSetFromProfile.getSpectrum(i).getWavelength(),spectrumSetFromProfile.getSpectrum(i).getSpectrum(),'-+')
-                plt.xlim(xMinMax[0],xMinMax[1])
-                plt.ylim(0,25000)
-            plt.xlabel('Wavelength [Angstroems]')
-            plt.ylabel('Flux [ADUs]')
-            plt.show()
-            plt.close(fig)
-            fig.clf()
-        
-        print 'writing SpectrumSet object'
-        pfsConfig = PfsConfig(pfsConfigId=None, tract=0, patch=0, fiberId=[0], ra=[0], dec=[0], catId=0, objId=0, fiberMag=0, mpsCen=0, filterNames=["g", "r", "i", "z", "y"])
-        pfsArm = PfsArm(arcRef.dataId['visit'], arcRef.dataId['spectrograph'], arcRef.dataId['arm'], pfsConfigId=pfsConfig.pfsConfigId, pfsConfig=pfsConfig)
-        pfsArm.lam = spectrumSetFromProfile.getAllWavelengths()
-        pfsArm.flux = spectrumSetFromProfile.getAllFluxes()
-        pfsArm.mask = spectrumSetFromProfile.getAllMasks()
-        pfsArm.sky = spectrumSetFromProfile.getAllSkies()
-#        pfsArm.covar = spectrumSetFromProfile.getAllVariances()
-        pfsArm.covar = spectrumSetFromProfile.getAllCovars()
+            arcExp = arcRef.get("postISRCCD", immediate=True)
+            self.log.info('arcExp = %s' % arcExp)
+            self.log.info('type(arcExp) = %s' % type(arcExp))
+            flatFiberTraceSet = makeFiberTraceSet(fiberTrace)
 
-        butler.put(pfsArm, 'pfsArm', arcRef.dataId )
-        return spectrumSetFromProfile
+            """ optimally extract arc spectra """
+            print 'extracting arc spectra'
+
+            """ read wavelength file """
+            hdulist = pyfits.open(wLenFile)
+            tbdata = hdulist[1].data
+            traceIdsTemp = np.ndarray(shape=(len(tbdata)), dtype='int')
+            xCenters = np.ndarray(shape=(len(tbdata)), dtype='float32')
+            yCenters = np.ndarray(shape=(len(tbdata)), dtype='float32')
+            wavelengths = np.ndarray(shape=(len(tbdata)), dtype='float32')
+            traceIdsTemp[:] = tbdata[:]['fiberNum']
+            traceIds = traceIdsTemp.astype('int32')
+            wavelengths[:] = tbdata[:]['pixelWave']
+            xCenters[:] = tbdata[:]['xc']
+            yCenters[:] = tbdata[:]['yc']
+
+            traceIdsUnique = np.unique(traceIds)
+
+            """ assign trace number to flatFiberTraceSet """
+            success = drpStella.assignITrace( flatFiberTraceSet, traceIds, xCenters, yCenters )
+            iTraces = np.ndarray(shape=flatFiberTraceSet.size(), dtype='intp')
+            for i in range( flatFiberTraceSet.size() ):
+                iTraces[i] = flatFiberTraceSet.getFiberTrace(i).getITrace()
+
+            if success == False:
+                print 'assignITrace FAILED'
+
+            myExtractTask = esTask.ExtractSpectraTask()
+            aperturesToExtract = [-1]
+            spectrumSetFromProfile = myExtractTask.run(arcExp, flatFiberTraceSet, aperturesToExtract)
+
+            for i in range(spectrumSetFromProfile.size()):
+                # THIS DOESN'T WORK, again a problem with changing getSpectrum()
+                spectrumSetFromProfile.getSpectrum(i).setITrace(flatFiberTraceSet.getFiberTrace(i).getITrace())
+    #            print 'spectrumSetFromProfile[',i,'].iTrace = ',spectrumSetFromProfile.getSpectrum(i).getITrace()
+
+    #        fig = plt.figure()
+    #        ax = fig.add_subplot(1, 1, 1)
+    #        for i in range(spectrumSetFromProfile.size()):
+    #            ax.plot(spectrumSetFromProfile.getSpectrum(i).getSpectrum(),'-+')
+    #            plt.xlim(1450,1600)
+    #            plt.ylim(0,8000)
+    #        plt.show()
+    #        plt.close(fig)
+    #        fig.clf()
+
+            """ read line list """
+            hdulist = pyfits.open(lineList)
+            tbdata = hdulist[1].data
+            lineListArr = np.ndarray(shape=(len(tbdata),2), dtype='float32')
+            lineListArr[:,0] = tbdata.field(0)
+            lineListArr[:,1] = tbdata.field(1)
+
+            dispCorControl = drpStella.DispCorControl()
+            dispCorControl.fittingFunction = self.config.function
+            dispCorControl.order = self.config.order
+            dispCorControl.searchRadius = self.config.searchRadius
+            dispCorControl.fwhm = self.config.fwhm
+            self.log.info('dispCorControl.fittingFunction = %s' % dispCorControl.fittingFunction)
+            self.log.info('dispCorControl.order = %d' % dispCorControl.order)
+            self.log.info('dispCorControl.searchRadius = %d' % dispCorControl.searchRadius)
+            self.log.info('dispCorControl.fwhm = %g' % dispCorControl.fwhm)
+
+            for i in range(spectrumSetFromProfile.size()):
+                spec = spectrumSetFromProfile.getSpectrum(i)
+                spec.setITrace(iTraces[i])
+                self.log.info('flatFiberTraceSet.getFiberTrace(%d).getITrace() = %d, spec.getITrace() = %d' %(i,flatFiberTraceSet.getFiberTrace(i).getITrace(), spec.getITrace()))
+                specSpec = spec.getSpectrum()
+                self.log.info('yCenters.shape = %d' % yCenters.shape)
+                self.log.info('specSpec.shape = %d' % specSpec.shape)
+                self.log.info('lineListArr.shape = [%d,%d]' % (lineListArr.shape[0], lineListArr.shape[1]))
+                self.log.info('type(specSpec) = %s: <%s>' % (type(specSpec),type(specSpec[0])))
+                self.log.info('type(lineListArr) = %s: <%s>' % (type(lineListArr),type(lineListArr[0][0])))
+                self.log.info('type(spec) = %s: <%s>: <%s>' % (type(spec),type(spec.getSpectrum()),type(spec.getSpectrum()[0])))
+
+                traceId = spec.getITrace()
+    #            print 'traceId = ',traceId
+                wLenTemp = np.ndarray( shape = traceIds.shape[0] / np.unique(traceIds).shape[0], dtype='float32' )
+                k = 0
+                l = -1
+                for j in range(traceIds.shape[0]):
+                    if traceIds[j] != l:
+                        l = traceIds[j]
+                    if traceIds[j] == traceIdsUnique[traceId]:
+                        wLenTemp[k] = wavelengths[j]
+                        k = k+1
+
+                """cut off both ends of wavelengths where is no signal"""
+                xCenter = flatFiberTraceSet.getFiberTrace(i).getFiberTraceFunction().xCenter
+                yCenter = flatFiberTraceSet.getFiberTrace(i).getFiberTraceFunction().yCenter
+                yLow = flatFiberTraceSet.getFiberTrace(i).getFiberTraceFunction().yLow
+                yHigh = flatFiberTraceSet.getFiberTrace(i).getFiberTraceFunction().yHigh
+                yMin = yCenter + yLow
+                yMax = yCenter + yHigh
+                self.log.info('fiberTrace %d: xCenter = %d' % (i, xCenter))
+                self.log.info('fiberTrace %d: yCenter = %d' % (i, yCenter))
+                self.log.info('fiberTrace %d: yLow = %d' % (i, yLow))
+                self.log.info('fiberTrace %d: yHigh = %d' % (i, yHigh))
+                self.log.info('fiberTrace %d: yMin = %d' % (i, yMin))
+                self.log.info('fiberTrace %d: yMax = %d' % (i, yMax))
+                wLen = wLenTemp[ yMin + self.config.nRowsPrescan : yMax + self.config.nRowsPrescan + 1]
+                wLenArr = np.ndarray(shape=wLen.shape, dtype='float32')
+                for j in range(wLen.shape[0]):
+                    wLenArr[j] = wLen[j]
+                wLenLines = lineListArr[:,0]
+                wLenLinesArr = np.ndarray(shape=wLenLines.shape, dtype='float32')
+                for j in range(wLenLines.shape[0]):
+                    wLenLinesArr[j] = wLenLines[j]
+                lineListPix = drpStella.createLineList(wLenArr, wLenLinesArr)
+                try:
+                    spec.identifyF(lineListPix, dispCorControl, 8)
+                except:
+                    e = sys.exc_info()[1]
+                    message = str.split(e.message, "\n")
+                    for k in range(len(message)):
+                        print "element",k,": <",message[k],">"
+                print "FiberTrace ",i,": spec.getDispCoeffs() = ",spec.getDispCoeffs()
+                print "FiberTrace ",i,": spec.getDispRms() = ",spec.getDispRms()
+                if spectrumSetFromProfile.setSpectrum(i, spec ):
+                    print 'setSpectrum for spectrumSetFromProfile[',i,'] done'
+                else:
+                    print 'setSpectrum for spectrumSetFromProfile[',i,'] failed'
+
+            if False:#disable plotting the data
+                xPixMinMax = np.ndarray(2, dtype='float32')
+                xPixMinMax[0] = 1000.
+                xPixMinMax[1] = 1600.
+                fig = plt.figure()
+                ax = fig.add_subplot(1, 1, 1)
+                for i in range(spectrumSetFromProfile.size()):
+                    ax.plot(spectrumSetFromProfile.getSpectrum(i).getSpectrum(),'-+')
+                    plt.xlim(xPixMinMax[0],xPixMinMax[1])
+                    plt.ylim(0,25000)
+                plt.xlabel('Pixel')
+                plt.ylabel('Flux [ADUs]')
+                plt.show()
+                plt.close(fig)
+                fig.clf()
+
+                xMinMax = drpStella.poly(xPixMinMax, spec.getDispCoeffs())
+                fig = plt.figure()
+                ax = fig.add_subplot(1, 1, 1)
+                for i in range(spectrumSetFromProfile.size()):
+                    ax.plot(spectrumSetFromProfile.getSpectrum(i).getWavelength(),spectrumSetFromProfile.getSpectrum(i).getSpectrum(),'-+')
+                    plt.xlim(xMinMax[0],xMinMax[1])
+                    plt.ylim(0,25000)
+                plt.xlabel('Wavelength [Angstroems]')
+                plt.ylabel('Flux [ADUs]')
+                plt.show()
+                plt.close(fig)
+                fig.clf()
+
+            print 'writing SpectrumSet object'
+            butler.put(spectrumSetFromProfile, 'spArm', arcRef.dataId )
