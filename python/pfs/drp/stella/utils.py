4f1ded499d056d36909f96740e7c5bee95083ced
Andreas Ritter <azuri.ritter@gmail.com>
2016-10-07 00:59:45 -0400

    Fix bugs in utils.py

--- python/pfs/drp/stella/utils.py
+++ python/pfs/drp/stella/utils.py
@@ -1,8 +1,10 @@
-from pfs.datamodel.pfsFiberTrace import PfsFiberTrace
+#from pfs.datamodel.pfsFiberTrace import PfsFiberTrace
 import pfs.drp.stella as drpStella
+import numpy as np
+import lsst.afw.image as afwImage
 
 def makeFiberTraceSet(pfsFiberTrace, maskedImage=None):
-    fts = drpStella.FiberTraceSet()
+    fts = drpStella.FiberTraceSetF()
     ftfc = drpStella.FiberTraceFunctionControl()
     ftf = drpStella.FiberTraceFunction()
     ftpfc = drpStella.FiberTraceProfileFittingControl()
@@ -36,19 +38,31 @@ def makeFiberTraceSet(pfsFiberTrace, maskedImage=None):
         ftf.yCenter = pfsFiberTrace.yCenter[iFt]
         ftf.yLow = pfsFiberTrace.yLow[iFt]
         ftf.yHigh = pfsFiberTrace.yHigh[iFt]
-        ftf.coefficients = pfsFiberTrace.coeffs[iFt]
-        ft = drpStella.FiberTrace()
+        print 'pfsFiberTrace.coeffs[iFt] = ',pfsFiberTrace.coeffs[iFt]
+        coeffs = np.ndarray(len(pfsFiberTrace.coeffs[iFt]), dtype=np.float64)
+        for iCoeff in range(coeffs.shape[0]):
+            coeffs[iCoeff] = pfsFiberTrace.coeffs[iFt][iCoeff]
+        print 'coeffs = ',coeffs.shape,': ',coeffs
+        ftf.coefficients = coeffs
+        ft = drpStella.FiberTraceF()
         if not ft.setFiberTraceFunction(ftf):
             raise RuntimeError("FiberTrace %d: Failed to set FiberTraceFunction" % iFt)
         if not ft.setFiberTraceProfileFittingControl(ftpfc):
             raise RuntimeError("FiberTrace %d: Failed to set FiberTraceProfileFittingControl" % iFt)
-        if not ft.setITrace(pfsFiberTrace.fiberId[iFt]-1):
-            raise RuntimeError("FiberTrace %d: Failed to set FiberId" % iFt)
+        ft.setITrace(pfsFiberTrace.fiberId[iFt]-1)
         if maskedImage != None:
             if not ft.createTrace(maskedImage):
                 raise RuntimeError("FiberTrace %d: Failed to create trace from maskedImage")
         profile = pfsFiberTrace.profiles[iFt]
-        if not ft.setProfile(profile):
+        trace = np.ndarray(shape=(ftf.yHigh - ftf.yLow + 1, profile.shape[1]), dtype=np.float32)
+        if not ft.setTrace(afwImage.makeMaskedImage(afwImage.ImageF(trace))):
+            raise RuntimeError("FiberTrace %d: Failed to set trace")
+        prof = np.ndarray(shape=(ftf.yHigh - ftf.yLow + 1, profile.shape[1]), dtype=np.float64)
+        for iRow in range(prof.shape[0]):
+            prof[iRow,:] = profile[ftf.yCenter + ftf.yLow + iRow,:]
+        if not ft.setProfile(afwImage.ImageD(prof)):
             raise RuntimeError("FiberTrace %d: Failed to set profile")
+        xCenters = drpStella.calculateXCenters(ftf)
+        ft.setXCenters(xCenters)
         fts.addFiberTrace(ft)
     return fts
