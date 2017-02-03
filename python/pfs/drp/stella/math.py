3fb7c2552fe6045f96cbfa93c126cf652995bdf2
Andreas Ritter <azuri.ritter@gmail.com>
2017-02-02 22:58:08 -0500

    math::getMeanStdXBins: Return 3 arrays instead of 1

--- python/pfs/drp/stella/math.py
+++ python/pfs/drp/stella/math.py
@@ -91,29 +91,29 @@ def getMeanStdXBins(x, y, binWidthX):
            NOTE that the array must be sorted in X.
     @param y in: np.ndarray of y values, same shape as x
     @param binWidthX in: width of center distance bins
-    @return array of shape[nBins,2,2] containing:
-            [:,0,:]: the X for start and end of the bin,
-            [:,1,:]: the mean and standard deviation of the y values in each bin
+    @return array of shape[nBins,2]: the X for start and end of the bin,
+            array of shape[nBins,1]: the mean of the y values in each bin
+            array of shape[nBins,1]: the standard deviation of the y values in each bin
     """
-    binMeanStdDevs = np.ndarray(
-                       shape=(int((x[x.shape[0]-1] - x[0]) / binWidthX) + 1,
-                              2,
-                              2),
-                       dtype=np.float32)
+    nBins = int((x[x.shape[0]-1] - x[0]) / binWidthX) + 1
+    binRanges = np.ndarray(shape=(nBins, 2), dtype=np.float32)
+    binRanges[:,:] = 0.
+    mean = np.ndarray(shape=(nBins), dtype=np.float32)
+    std = np.ndarray(shape=(nBins), dtype=np.float32)
     binRange = [x[0], x[0]+binWidthX]
     nPix = 0
-    for iBin in range(binMeanStdDevs.shape[0]):
+    for iBin in range(mean.shape[0]):
         iPix = nPix
-        binMeanStdDevs[iBin,:,:] = 0.
         while ((iPix < x.shape[0])
                and
                (x[iPix if iPix < x.shape[0]
                         else x.shape[0] - 1] < binRange[1])):
             iPix += 1
-        binMeanStdDevs[iBin, 0, :] = [binRange[0], binRange[1]]
+        binRanges[iBin, :] = [binRange[0], binRange[1]]
         if iPix > nPix:
-            binMeanStdDevs[iBin, 1, :] = [np.mean(y[nPix:iPix]), np.std(y[nPix:iPix])]
+            mean[iBin] = np.mean(y[nPix:iPix])
+            std[iBin] = np.std(y[nPix:iPix])
         nPix = iPix
         binRange[0] = binRange[1]
         binRange[1] += binWidthX
-    return binMeanStdDevs
+    return binRanges, mean, std
