772df6e31fcd13aa41e42da2be4eda0589cda6c6
Andreas Ritter <azuri.ritter@gmail.com>
2017-02-07 23:42:03 -0500

    Add config files for createFlatFiberTraceProfileTask and
    findAndTraceAperturesTask

---
+++ config/findAndTraceApertures.py
@@ -0,0 +1,14 @@
+config.interpolation = "POLYNOMIAL"
+config.order = 5
+config.xLow = -5.5
+config.xHigh = 5.5
+config.nPixCutLeft = 1
+config.nPixCutRight = 1
+config.apertureFWHM = 2.5
+config.signalThreshold = 120.
+config.nTermsGaussFit = 3
+config.saturationLevel = 65000.0
+config.minLength = 3000
+config.maxLength = 4096
+config.nLost = 10
+
