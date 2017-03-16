968f94a377f385bebf266c5c9dfed131de8aff04
Andreas Ritter <azuri.ritter@gmail.com>
2017-03-16 14:38:03 -0400

    reduceArcRefSpecTask: Reduce line length to 110 characters

--- python/pfs/drp/stella/reduceArcRefSpecTask.py
+++ python/pfs/drp/stella/reduceArcRefSpecTask.py
@@ -13,24 +13,85 @@ from pfs.drp.stella.utils import makeFiberTraceSet
 
 class ReduceArcRefSpecConfig(Config):
     """Configuration for reducing arc images"""
-    function = Field( doc = "Function for fitting the dispersion", dtype=str, default="POLYNOMIAL" );
-    order = Field( doc = "Fitting function order", dtype=int, default = 5 );
-    searchRadius = Field( doc = "Radius in pixels relative to line list to search for emission line peak", dtype = int, default = 2 );
-    fwhm = Field( doc = "FWHM of emission lines", dtype=float, default = 2.6 );
-    radiusXCor = Field( doc = "Radius in pixels in which to cross correlate a spectrum relative to the reference spectrum", dtype = int, default = 50 );
-    lengthPieces = Field( doc = "Length of pieces of spectrum to match to reference spectrum by stretching and shifting", dtype = int, default = 500 );
-    nCalcs = Field( doc = "Number of iterations > spectrumLength / lengthPieces, e.g. spectrum length is 3800 pixels, <lengthPieces> = 500, <nCalcs> = 15: run 1: pixels 0-499, run 2: 249-749,...", dtype = int, default = 15 );
-    stretchMinLength = Field( doc = "Minimum length to stretched pieces to (< lengthPieces)", dtype = int, default = 460 );
-    stretchMaxLength = Field( doc = "Maximum length to stretched pieces to (> lengthPieces)", dtype = int, default = 540 );
-    nStretches = Field( doc = "Number of stretches between <stretchMinLength> and <stretchMaxLength>", dtype = int, default = 80 );
-    refSpec = Field( doc = "reference reference spectrum including path", dtype = str, default=os.path.join(getPackageDir("obs_pfs"), "pfs/arcSpectra/refSpec_CdHgKrNeXe_red.fits"));
-    lineList = Field( doc = "reference line list including path", dtype = str, default=os.path.join(getPackageDir("obs_pfs"), "pfs/lineLists/CdHgKrNeXe_red.fits"));
+    function = Field(
+        doc = "Function for fitting the dispersion",
+        dtype = str,
+        default = "POLYNOMIAL"
+    )
+    order = Field(
+        doc = "Fitting function order",
+        dtype = int,
+        default = 5
+    )
+    searchRadius = Field(
+        doc = "Radius in pixels relative to line list to search for emission line peak",
+        dtype = int,
+        default = 2
+    )
+    fwhm = Field(
+        doc = "FWHM of emission lines",
+        dtype = float,
+        default = 2.6,
+    )
+    radiusXCor = Field(
+        doc = "Radius in pixels in which to cross correlate a spectrum relative to the reference spectrum",
+        dtype = int,
+        default = 50,
+    )
+    lengthPieces = Field(
+        doc = "Length of pieces of spectrum to match to reference spectrum by stretching and shifting",
+        dtype = int,
+        default = 500,
+    )
+    docString = "Number of iterations > spectrumLength / lengthPieces, e.g. spectrum length is 3800 pixels, "
+    docString += "<lengthPieces> = 500, <nCalcs> = 15: run 1: pixels 0-499, run 2: 249-749,..."
+    nCalcs = Field(
+        doc = docString,
+        dtype = int,
+        default = 15,
+    )
+    stretchMinLength = Field(
+        doc = "Minimum length to stretched pieces to (< lengthPieces)",
+        dtype = int,
+        default = 460,
+    )
+    stretchMaxLength = Field(
+        doc = "Maximum length to stretched pieces to (> lengthPieces)",
+        dtype = int,
+        default = 540,
+    )
+    nStretches = Field(
+        doc = "Number of stretches between <stretchMinLength> and <stretchMaxLength>",
+        dtype = int,
+        default = 80,
+    )
+    refSpec = Field(
+        doc = "reference reference spectrum including path",
+        dtype = str,
+        default = os.path.join(
+            getPackageDir("obs_pfs"),
+            "pfs/arcSpectra/refSpec_CdHgKrNeXe_red.fits",
+        ),
+    )
+    lineList = Field(
+        doc = "reference line list including path",
+        dtype = str,
+        default = os.path.join(
+            getPackageDir("obs_pfs"),
+            "pfs/lineLists/CdHgKrNeXe_red.fits",
+        ),
+    )
 
 class ReduceArcRefSpecTaskRunner(TaskRunner):
     """Get parsed values into the ReduceArcTask.run"""
     @staticmethod
     def getTargetList(parsedCmd, **kwargs):
-        return [dict(expRefList=parsedCmd.id.refList, butler=parsedCmd.butler, refSpec=parsedCmd.refSpec, lineList=parsedCmd.lineList)]
+        return [dict(expRefList=parsedCmd.id.refList,
+                     butler=parsedCmd.butler,
+                     refSpec=parsedCmd.refSpec,
+                     lineList=parsedCmd.lineList,
+                    ),
+        ]
 
     def __call__(self, args):
         task = self.TaskClass(config=self.config, log=self.log)
@@ -62,10 +123,19 @@ class ReduceArcRefSpecTask(CmdLineTask):
     @classmethod
     def _makeArgumentParser(cls, *args, **kwargs):
         parser = ArgumentParser(name=cls._DefaultName)
-        parser.add_id_argument("--id", datasetType="postISRCCD",
-                               help="input identifiers, e.g., --id visit=123 ccd=4")
-        parser.add_argument("--refSpec", help='directory and name of reference spectrum')
-        parser.add_argument("--lineList", help='directory and name of line list')
+        parser.add_id_argument(
+            "--id",
+            datasetType="postISRCCD",
+            help="input identifiers, e.g., --id visit=123 ccd=4",
+        )
+        parser.add_argument(
+            "--refSpec",
+            help='directory and name of reference spectrum',
+        )
+        parser.add_argument(
+            "--lineList",
+            help='directory and name of line list',
+        )
         return parser# ReduceArcArgumentParser(name=cls._DefaultName, *args, **kwargs)
 
     def run(self, expRefList, butler, refSpec=None, lineList=None, immediate=True):
@@ -154,24 +224,55 @@ class ReduceArcRefSpecTask(CmdLineTask):
             for i in range(spectrumSetFromProfile.size()):
                 spec = spectrumSetFromProfile.getSpectrum(i)
                 specSpec = spectrumSetFromProfile.getSpectrum(i).getSpectrum()
-                self.log.info('calibrating spectrum %d: xCenter = %f' % (i,flatFiberTraceSet.getFiberTrace(i).getFiberTraceFunction().xCenter))
+                self.log.info('calibrating spectrum %d: xCenter = %f' %
+                    (i,
+                     flatFiberTraceSet.getFiberTrace(i).getFiberTraceFunction().xCenter,
+                    ))
                 self.log.debug('specSpec.shape = %d' % specSpec.shape)
-                self.log.debug('lineListArr.shape = [%d,%d]' % (lineListArr.shape[0], lineListArr.shape[1]))
+                self.log.debug('lineListArr.shape = [%d,%d]' %
+                    (lineListArr.shape[0],
+                     lineListArr.shape[1],
+                    ))
                 self.log.debug('type(specSpec) = %s: <%s>' % (type(specSpec),type(specSpec[0])))
                 self.log.debug('type(refSpecArr) = %s: <%s>' % (type(refSpecArr),type(refSpecArr[0])))
                 self.log.debug('type(lineListArr) = %s: <%s>' % (type(lineListArr),type(lineListArr[0][0])))
-                result = drpStella.stretchAndCrossCorrelateSpecFF(specSpec, refSpecArr, lineListArr, dispCorControl)
-                self.log.debug('type(result.lineList) = %s: <%s>: <%s>' % (type(result.lineList),type(result.lineList[0]),type(result.lineList[0][0])))
-                self.log.debug('type(spectrumSetFromProfile.getSpectrum(i)) = %s: <%s>: <%s>' % (type(spectrumSetFromProfile.getSpectrum(i)),type(spectrumSetFromProfile.getSpectrum(i).getSpectrum()),type(spectrumSetFromProfile.getSpectrum(i).getSpectrum()[0])))
+                result = drpStella.stretchAndCrossCorrelateSpecFF(
+                    specSpec,
+                    refSpecArr,
+                    lineListArr,
+                    dispCorControl
+                )
+                self.log.debug('type(result.lineList) = %s: <%s>: <%s>' %
+                    (type(result.lineList),
+                     type(result.lineList[0]),
+                     type(result.lineList[0][0]),
+                    )
+                )
+                self.log.debug('type(spectrumSetFromProfile.getSpectrum(i)) = %s: <%s>: <%s>' %
+                    (type(spectrumSetFromProfile.getSpectrum(i)),
+                     type(spectrumSetFromProfile.getSpectrum(i).getSpectrum()),
+                     type(spectrumSetFromProfile.getSpectrum(i).getSpectrum()[0]),
+                    )
+                )
                 for j in range(result.lineList.shape[0]):
-                    self.log.debug('result.lineList[%d][*] = %f, %f' % (j,result.lineList[j][0],result.lineList[j][1]))
+                    self.log.debug('result.lineList[%d][*] = %f, %f' %
+                        (j,
+                         result.lineList[j][0],
+                         result.lineList[j][1],
+                        )
+                    )
                 spec.identifyF(result.lineList, dispCorControl, 8)
                 if spectrumSetFromProfile.setSpectrum(i, spec ):
                     self.log.debug('setSpectrum for spectrumSetFromProfile[%d] done' % i)
                 else:
                     self.log.warn('setSpectrum for spectrumSetFromProfile[%d] failed' % i)
                 for j in range(specSpec.shape[0]):
-                    self.log.debug('spectrum %d: spec.getWavelength()[%d] = %f' % (i,j,spec.getWavelength()[j]))
+                    self.log.debug('spectrum %d: spec.getWavelength()[%d] = %f' %
+                        (i,
+                         j,
+                         spec.getWavelength()[j],
+                        )
+                    )
 
             if False:
                 xPixMinMax = np.ndarray(2, dtype='float32')
@@ -194,7 +295,11 @@ class ReduceArcRefSpecTask(CmdLineTask):
                 fig = plt.figure()
                 ax = fig.add_subplot(1, 1, 1)
                 for i in range(spectrumSetFromProfile.size()):
-                    ax.plot(spectrumSetFromProfile.getSpectrum(i).getWavelength(),spectrumSetFromProfile.getSpectrum(i).getSpectrum(),'-+')
+                    ax.plot(
+                        spectrumSetFromProfile.getSpectrum(i).getWavelength(),
+                        spectrumSetFromProfile.getSpectrum(i).getSpectrum(),
+                        '-+',
+                    )
                     plt.xlim(xMinMax[0],xMinMax[1])
                     plt.ylim(0,25000)
                 plt.xlabel('Wavelength [Angstroems]')
