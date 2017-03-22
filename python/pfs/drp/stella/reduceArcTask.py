9187a411bf74e033d68f4294d90d07ecc73b2535
Andreas Ritter <azuri.ritter@gmail.com>
2017-03-22 15:50:45 -0400

    reduceArcTask: Wrap createLineList in try except block and
    fix the way error messages in try/except blocks are handled
    by giving the Exception a name (e) and writing out a lsst
    log warn message with e.message

--- python/pfs/drp/stella/reduceArcTask.py
+++ python/pfs/drp/stella/reduceArcTask.py
@@ -5,12 +5,14 @@ import sys
 import pfs.drp.stella.extractSpectraTask as esTask
 import pfs.drp.stella as drpStella
 from pfs.drp.stella.utils import makeFiberTraceSet
+import lsst.log as log
 from lsst.utils import getPackageDir
 from lsst.pex.config import Config, Field
 from lsst.pipe.base import Struct, TaskRunner, ArgumentParser, CmdLineTask
 import numpy as np
 from astropy.io import fits as pyfits
 from pfs.drp.stella.datamodelIO import spectrumSetToPfsArm, PfsArmIO
+import traceback
 
 class ReduceArcConfig(Config):
     """Configuration for reducing arc images"""
@@ -170,16 +172,23 @@ class ReduceArcTask(CmdLineTask):
                     wLenLinesArr[j] = wLenLines[j]
                 lineListPix = drpStella.createLineList(wLenArr, wLenLinesArr)
 
+                log.logging.getLogger("pfs::drp::stella::math::createLineList").setLevel(log.DEBUG)
+                try:
+                    lineListPix = drpStella.createLineList(wLenArr, wLenLinesArr)
+                except Exception as e:
+                    self.log.warn("Error occured during createLineList: %s" % (e.message))
+
                 # Idendify emission lines and fit dispersion
                 try:
                     spec.identifyF(lineListPix, dispCorControl, 8)
-                except:
-                    e = sys.exc_info()[1]
-                    message = str.split(e.message, "\n")
-                    for k in range(len(message)):
-                        print "element",k,": <",message[k],">"
-                print "FiberTrace ",i,": spec.getDispCoeffs() = ",spec.getDispCoeffs()
-                print "FiberTrace ",i,": spec.getDispRms() = ",spec.getDispRms()
+                except Exception as e:
+                    self.log.warn("Error occured during identify: %s" % (e.message))
+
+                self.log.trace("FiberTrace %d: spec.getDispCoeffs() = %s"
+                               % (i, np.array_str(spec.getDispCoeffs())))
+                self.log.info("FiberTrace %d: spec.getDispRms() = %f"
+                              % (i, spec.getDispRms()))
+
                 if spectrumSetFromProfile.setSpectrum(i, spec ):
                     print 'setSpectrum for spectrumSetFromProfile[',i,'] done'
                 else:
