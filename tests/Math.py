db47f71f7f53b30eb4685dbc9690209f41ce420b
Andreas Ritter <azuri.ritter@gmail.com>
2017-07-13 16:21:34 -0400

    Math.py: Add tests for GaussFit, makeArtificialSpectrum,  measureLinesInWavelengthSpace

--- tests/Math.py
+++ tests/Math.py
@@ -12,8 +12,11 @@ import unittest
 
 import numpy as np
 
+import lsst.log as log
 import lsst.utils.tests as tests
 import pfs.drp.stella as drpStella
+from pfs.drp.stella.math import gauss, makeArtificialSpectrum
+from pfs.drp.stella.utils import measureLinesInPixelSpace, removeBadLines
 
 class MathTestCase(tests.TestCase):
     """A test case for Stella math functions"""
@@ -68,6 +71,137 @@ class MathTestCase(tests.TestCase):
         sortedIndices = drpStella.sortIndices(int1DArr)
         for ind, val in zip(sortedIndices, [2,4,7,8,6,0,5,9,1,3]):
             self.assertEqual(ind, val)
+
+    def testGaussFit(self):
+        guess = drpStella.GaussCoeffs()
+        guess.strength, guess.mu, guess.sigma = [10.0, 3.0, 1.0]
+        x = np.arange(7)
+        y = gauss(x,guess)
+        self.assertAlmostEqual(guess.strength, np.max(y))
+        gaussFitResult = drpStella.math.gaussFit(x, y, guess)
+        coeffs = drpStella.GaussCoeffs()
+        coeffs.strength, coeffs.mu, coeffs.sigma = [gaussFitResult[0],
+                                                    gaussFitResult[1],
+                                                    gaussFitResult[2]]
+        eCoeffs = drpStella.GaussCoeffs()
+        eCoeffs.strength, eCoeffs.mu, eCoeffs.sigma = [gaussFitResult[3],
+                                                       gaussFitResult[4],
+                                                       gaussFitResult[5]]
+        self.assertAlmostEqual(guess.strength, coeffs.strength)
+        self.assertAlmostEqual(guess.mu, coeffs.mu)
+        self.assertAlmostEqual(guess.sigma, coeffs.sigma)
+        self.assertAlmostEqual(0.0, eCoeffs.strength)
+        self.assertAlmostEqual(0.0, eCoeffs.mu)
+        self.assertAlmostEqual(0.0, eCoeffs.sigma)
+
+    def testMakeArtificialSpectrum(self):
+        lines = []
+        line = drpStella.NistLine()
+        line.element='Hg'
+        line.flags='g'
+        line.id=0
+        line.ion='I'
+        line.sources='abc'
+        line.predictedStrength = 10.0
+        line.laboratoryWavelength=30.0
+        lines.append(line)
+        lambdaPix = np.arange(100)
+        fwhm = 1.1774 # sigma = 1
+        lambdaMin = 40.0 # line outside range
+        lambdaMax = 80.0
+        spec = makeArtificialSpectrum(lambdaPix, lines, lambdaMin, lambdaMax, fwhm)
+        self.assertAlmostEqual(np.max(spec),0.0)
+
+        lambdaMin = 0.0 # line outside range
+        lambdaMax = 20.0
+        spec = makeArtificialSpectrum(lambdaPix, lines, lambdaMin, lambdaMax, fwhm)
+        self.assertAlmostEqual(np.max(spec),0.0)
+
+        lambdaMin = 10.0 # line outside range
+        lambdaMax = 50.0
+        spec = makeArtificialSpectrum(lambdaPix, lines, lambdaMin, lambdaMax, fwhm)
+        specMax = np.max(spec)
+        self.assertAlmostEqual(specMax,10.0)
+        self.assertAlmostEqual(specMax,spec[30])
+
+        lambdaMin = 0.0 # line outside range
+        lambdaMax = 0.0
+        spec = makeArtificialSpectrum(lambdaPix, lines, lambdaMin, lambdaMax, fwhm)
+        specMax = np.max(spec)
+        self.assertAlmostEqual(specMax,10.0)
+        self.assertAlmostEqual(specMax,spec[30])
+
+    def testMeasureLinesInPixelSpace(self):
+        lines = []
+        lineA = drpStella.NistLine()
+        lineA.element='Hg'
+        lineA.flags=''
+        lineA.id=0
+        lineA.ion='I'
+        lineA.sources='a'
+        lineA.predictedStrength = 10.0
+        lineA.laboratoryWavelength=10.0
+        lines.append(lineA)
+
+        lineB = drpStella.NistLine()
+        lineB.element='Hg'
+        lineB.flags=''
+        lineB.id=1
+        lineB.ion='II'
+        lineB.sources='a'
+        lineB.predictedStrength = 10.0
+        lineB.laboratoryWavelength=30.0
+        lines.append(lineB)
+
+        lineC = drpStella.NistLine()
+        lineC.element='Hg'
+        lineC.flags=''
+        lineC.id=2
+        lineC.ion='II'
+        lineC.sources='a'
+        lineC.predictedStrength = 20.0
+        lineC.laboratoryWavelength=31.0
+        lines.append(lineC)
+
+        lineD = drpStella.NistLine()
+        lineD.element='Hg'
+        lineD.flags='a'
+        lineD.id=3
+        lineD.ion='I'
+        lineD.sources='a'
+        lineD.predictedStrength = 10.0
+        lineD.laboratoryWavelength=40.0
+        lines.append(lineD)
+
+        lambdaPix = np.arange(50.0)
+        fwhm = 2.0
+        fluxPix = makeArtificialSpectrum(lambdaPix, lines, fwhm=fwhm)
+        linesMeas = measureLinesInPixelSpace(lines, lambdaPix, fluxPix, fwhm)
+        for i in np.arange(0,len(linesMeas),3):
+            self.assertAlmostEqual(linesMeas[i].pixelPosPredicted,
+                                   linesMeas[i].nistLine.laboratoryWavelength)
+            self.assertAlmostEqual(linesMeas[i].gaussCoeffsPixel.mu,
+                                   linesMeas[i].nistLine.laboratoryWavelength)
+            self.assertAlmostEqual(linesMeas[i].gaussCoeffsPixel.strength,
+                                   linesMeas[i].nistLine.predictedStrength)
+            self.assertAlmostEqual(linesMeas[i].gaussCoeffsPixel.sigma,
+                                   fwhm / 1.1774)
+
+        logger = log.Log.getLogger("removeBadLines")
+        logger.setLevel(log.WARN)
+        goodLines = removeBadLines(linesMeas,
+                                   fluxPix,
+                                   False,
+                                   fwhm,
+                                   2.0,
+                                   1.0,
+                                   8.0,
+                                   100.0)
+        self.assertEqual(len(goodLines), 1)
+        self.assertEqual(linesMeas[0].flags, 'g')
+        self.assertEqual(linesMeas[1].flags, 'b')
+        self.assertEqual(linesMeas[2].flags, 'b')
+        self.assertEqual(linesMeas[3].flags, 'n')
 
 #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
