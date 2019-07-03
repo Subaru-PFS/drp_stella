0e7f9b9a7df1b0421b35cf8eaf1b97da084c9e35
nakakeigo <keigo.nakamura@ipmu.jp>
2019-07-03 04:20:57 -0400

    sole conflict

--- tests/test_WavelengthFitData.py
+++ tests/test_WavelengthFitData.py
@@ -18,19 +18,20 @@ class WavelengthFitDataTestCase(lsst.utils.tests.TestCase):
     def setUp(self):
         self.length = 128
         self.fiberId = np.arange(123, 456, 78, dtype=np.int32)
-        self.pixels = np.random.uniform(size=self.fiberId.shape)*self.length
-        self.pixelsErr = np.random.uniform(size=self.fiberId.shape)*self.length
-        self.refpixels = np.random.uniform(size=self.fiberId.shape)*self.length
+        self.fitPixelPos = np.random.uniform(size=self.fiberId.shape)*self.length
+        self.nominalPixelPos = np.random.uniform(size=self.fiberId.shape)*self.length
+        self.fitPixelPosErr = np.random.uniform(size=self.fiberId.shape)*self.length*0.01
         self.wlMin = 600.0  # nm
         self.wlMax = 900.0  # nm
         scale = self.wlMax - self.wlMin
-        self.measuredWavelength = np.random.uniform(size=self.fiberId.shape)*scale + self.wlMin
-        self.measuredWavelengthErr = np.random.uniform(size=self.fiberId.shape)*scale + self.wlMin
-        self.refWavelength = np.random.uniform(size=self.fiberId.shape)*scale + self.wlMin
-        self.status = np.random.randint(2,size=self.fiberId.shape)
-        self.wavelengthCorr = np.random.uniform(size=self.fiberId.shape)
-        self.actualWavelength = np.random.uniform(size=self.fiberId.shape)*scale + self.wlMin 
-        self.lines = [LineData(*args) for args in zip(self.fiberId, self.refWavelength, self.refpixels, self.pixels, self.measuredWavelength, self.pixelsErr, self.measuredWavelengthErr, self.wavelengthCorr, self.status)]
+        self.fitWavelength = np.random.uniform(size=self.fiberId.shape)*scale + self.wlMin
+        self.actualWavelength = np.random.uniform(size=self.fiberId.shape)*scale + self.wlMin
+        self.fitWavelengthErr = (np.random.uniform(size=self.fiberId.shape)*scale + self.wlMin)*0.0
+        self.wavelengthCorr = (np.random.uniform(size=self.fiberId.shape)*scale + self.wlMin)*0.01
+        self.reflines = np.random.uniform(size=self.fiberId.shape)*scale + self.wlMin
+        self.status = np.random.uniform(size=self.fiberId.shape)
+        self.lines = [LineData(*args) for args in zip(self.fiberId, self.reflines,self.nominalPixelPos, self.fitPixelPos, self.fitWavelength, self.fitPixelPosErr, self.fitWavelengthErr,self.wavelengthCorr, self.status)]
+
 
     def assertWavelengthFitData(self, wlFitData, atol=0.0):
         """Check that the WavelengthFitData is what we expect
@@ -45,15 +46,15 @@ class WavelengthFitDataTestCase(lsst.utils.tests.TestCase):
         """
         self.assertEqual(len(wlFitData), len(self.fiberId))
         self.assertFloatsEqual(wlFitData.fiberId, self.fiberId)
-        self.assertFloatsEqual(wlFitData.nominalPixelPos, self.pixels)
-        self.assertFloatsAlmostEqual(wlFitData.fitWavelength, self.measuredWavelength, atol=atol)
+        self.assertFloatsEqual(wlFitData.nominalPixelPos, self.nominalPixelPos)
+        self.assertFloatsAlmostEqual(wlFitData.fitWavelength, self.fitWavelength, atol=atol)
 
     def testBasic(self):
         """Test basic functionality"""
         wlFitData = WavelengthFitData(self.lines)
         self.assertWavelengthFitData(wlFitData)
+        residuals = self.fitWavelength - self.reflines
 
-        residuals = self.measuredWavelength - self.refWavelength        
         self.assertFloatsEqual(wlFitData.residuals(), residuals)
         self.assertEqual(wlFitData.mean(), residuals.mean())
         self.assertEqual(wlFitData.stdev(), residuals.std())
@@ -79,13 +80,13 @@ class WavelengthFitDataTestCase(lsst.utils.tests.TestCase):
             line = ReferenceLine("fake")
             line.status = ReferenceLine.Status.FIT
             line.wavelength = self.actualWavelength[ii]
-            line.fitPosition = self.pixels[ii]
+            line.fitPosition = self.nominalPixelPos[ii]
             ss.setReferenceLines([line])
-            knots = np.array([-1, 0, self.pixels[ii], self.length, self.length + 1], dtype=np.float32)
+            knots = np.array([-1, 0, self.nominalPixelPos[ii], self.length, self.length + 1], dtype=np.float32)
             centerKnots.append(knots)
             wavelengthKnots.append(knots)
             centerValues.append(np.array([12.34, 123.45, 234.56, 345.67, 456.78], dtype=np.float32))
-            wavelengthValues.append(np.array([self.wlMin - 1, self.wlMin, self.measuredWavelength[ii],
+            wavelengthValues.append(np.array([self.wlMin - 1, self.wlMin, self.fitWavelength[ii],
                                               self.wlMax, self.wlMax + 1],
                                              dtype=np.float32))
             linedata=LineData(ss.fiberId,line,line.fitPosition, 0, line.fitPosition, 0, 0, 0,line.status)
@@ -94,6 +95,7 @@ class WavelengthFitDataTestCase(lsst.utils.tests.TestCase):
         bbox = lsst.afw.geom.Box2I(lsst.afw.geom.Point2I(0, 0),
                                    lsst.afw.geom.Extent2I(self.length, self.length))
         detMap = DetectorMap(bbox, self.fiberId, centerKnots, centerValues, wavelengthKnots, wavelengthValues)
+
  
         wlFitData = WavelengthFitData.fromSpectrumSet(Lines)
         self.assertWavelengthFitData(wlFitData, atol=1.0e-4)
