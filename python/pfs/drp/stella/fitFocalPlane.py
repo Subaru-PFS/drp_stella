8138672a20cac5cd9afbea0a412038a74f6ab3c5
Paul Price <price@astro.princeton.edu>
2020-09-30 16:59:27 -0400

    FitFocalPlaneTask: extend to respect masks and variances
    FitFocalPlaneTask has been a placeholder, and its shortcomings have started
    to be felt in the form of false emission lines from a single bad pixel in
    a sky fiber. This commit upgrades its capabilities, so that it implements
    rejection and handles variances (input and output): essentially, it now
    does a weighted coadd.
    
    This invalidates any existing sky1d and fluxCal products on disk, as the
    I/O has changed to support the mask and variance. The API has also changed,
    so updated SubtractSky1dTask and MeasureFluxCalibrationTask to use the new
    capabilities.
    
    For all of this, the implementation is still somewhat of a placeholder,
    since it doesn't do any fitting across the focal plane.

--- python/pfs/drp/stella/fitFocalPlane.py
+++ python/pfs/drp/stella/fitFocalPlane.py
@@ -1,11 +1,14 @@
 import types
 
 import numpy as np
-import scipy.interpolate
 import astropy.io.fits
 
-from lsst.pex.config import Config
-from lsst.pipe.base import Task
+from lsst.pex.config import Config, Field
+from lsst.pipe.base import Task, Struct
+
+from pfs.drp.stella.datamodel.interpolate import interpolateFlux, interpolateMask
+
+import lsstDebug
 
 
 class FocalPlaneFunction(types.SimpleNamespace):
@@ -16,16 +19,26 @@ class FocalPlaneFunction(types.SimpleNamespace):
 
     Parameters
     ----------
-    array : `numpy.ndarray`
-        Constant vector to use.
+    wavelength : `numpy.ndarray` of `float`, shape ``(N,)``
+        Wavelengths for each value.
+    value : `numpy.ndarray` of `float`, shape ``(N,)``
+        Value at each wavelength.
+    mask : `numpy.ndarray` of `bool`, shape ``(N,)``
+        Indicate whether values should be ignored.
+    variance : `numpy.ndarray` of `float`, shape ``(N,)``
+        Variance in value at each wavelength.
     """
-    def __init__(self, wavelength, vector):
-        interpolator = scipy.interpolate.interp1d(wavelength, vector, kind='linear', bounds_error=False,
-                                                  fill_value=0, copy=True, assume_sorted=True)
-        super().__init__(wavelength=wavelength, vector=vector, interpolator=interpolator)
+    def __init__(self, wavelength, value, mask, variance):
+        super().__init__(wavelength=wavelength, value=value, mask=mask, variance=variance)
 
     def __call__(self, wavelengths, positions):
         """Evaluate the function at the provided positions
+
+        We interpolate the variance without doing any of the usual error
+        propagation. Since there is likely some amount of unknown covariance
+        (if only from resampling to a common wavelength scale), following the
+        usual error propagation formulae as if there is no covariance would
+        artificially suppress the noise estimates.
 
         Parameters
         ----------
@@ -36,14 +49,25 @@ class FocalPlaneFunction(types.SimpleNamespace):
 
         Returns
         -------
-        result : list (length ``N``) of `numpy.ndarray` of shape ``(M)``
+        values : list (length ``N``) of `numpy.ndarray` of `float`, shape ``(M)``
             Vector function evaluated at each position.
+        masks : list (length ``N``) of `numpy.ndarray` of `bool`, shape ``(M)``
+            Indicates whether the value at each position is valid.
+        variances : list (length ``N``) of `numpy.ndarray` of `float`, shape ``(M)``
+            Variances for each position.
         """
         assert len(wavelengths) == len(positions)
-        doResample = (wl.shape != self.wavelength.shape or not np.all(wl == self.wavelength) for
-                      wl in wavelengths)
-        return [self.interpolator(wl) if resamp else self.vector for
-                wl, resamp in zip(wavelengths, doResample)]
+
+        doResample = [wl.shape != self.wavelength.shape or not np.all(wl == self.wavelength) for
+                      wl in wavelengths]
+
+        values = [interpolateFlux(self.wavelength, self.value, wl) if resamp else self.value for
+                  wl, resamp in zip(wavelengths, doResample)]
+        masks = [interpolateMask(self.wavelength, self.mask, wl).astype(bool) if resamp else self.mask for
+                 wl, resamp in zip(wavelengths, doResample)]
+        variances = [interpolateFlux(self.wavelength, self.variance, wl) if resamp else self.variance for
+                     wl, resamp in zip(wavelengths, doResample)]
+        return Struct(values=values, masks=masks, variances=variances)
 
     @classmethod
     def readFits(cls, filename):
@@ -61,8 +85,10 @@ class FocalPlaneFunction(types.SimpleNamespace):
         """
         with astropy.io.fits.open(filename) as fits:
             wavelength = fits["WAVELENGTH"].data
-            vector = fits["VECTOR"].data
-        return cls(wavelength, vector)
+            value = fits["VALUE"].data
+            mask = fits["MASK"].data.astype(bool)
+            variance = fits["VARIANCE"].data
+        return cls(wavelength, value, mask, variance)
 
     def writeFits(self, filename):
         """Write to FITS file
@@ -74,26 +100,32 @@ class FocalPlaneFunction(types.SimpleNamespace):
         """
         fits = astropy.io.fits.HDUList()
         fits.append(astropy.io.fits.ImageHDU(self.wavelength, name="WAVELENGTH"))
-        fits.append(astropy.io.fits.ImageHDU(self.vector, name="VECTOR"))
+        fits.append(astropy.io.fits.ImageHDU(self.value, name="VALUE"))
+        fits.append(astropy.io.fits.ImageHDU(self.mask.astype(np.uint8), name="MASK"))
+        fits.append(astropy.io.fits.ImageHDU(self.variance, name="VARIANCE"))
         with open(filename, "wb") as fd:
             fits.writeto(fd)
 
 
 class FitFocalPlaneConfig(Config):
     """Configuration for FitFocalPlaneTask"""
-    pass
+    rejIterations = Field(dtype=int, default=3, doc="Number of rejection iterations")
+    rejThreshold = Field(dtype=float, default=3.0, doc="Rejection threshold (sigma)")
 
 
 class FitFocalPlaneTask(Task):
     """Fit a vector function over the focal plane
 
-    This implementation is a placeholder, as it simply averages the input
-    vectors instead of doing any real fitting or rejection of outliers, and
-    no attention is paid to the position of the fibers on the focal plane.
+    This implementation is a placeholder, as no attention is paid to the
+    position of the fibers on the focal plane.
     """
     ConfigClass = FitFocalPlaneConfig
 
-    def run(self, wavelength, vectors, errors, masks, fiberIdList, pfsConfig):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.debugInfo = lsstDebug.Info(__name__)
+
+    def run(self, wavelength, vectors, variances, masks, fiberIdList, pfsConfig):
         """Fit a vector function as a function of wavelength over the focal plane
 
         Note that this requires that all the input vectors have the same
@@ -105,8 +137,8 @@ class FitFocalPlaneTask(Task):
             Wavelength values, of length ``N``.
         vectors : `numpy.ndarray` of shape ``(M, N)``
             Measured vectors of length ``N`` for ``M`` positions.
-        errors : `numpy.ndarray` of shape ``(M, N)``
-            Errors in the measured vectors.
+        variances : `numpy.ndarray` of shape ``(M, N)``
+            Variances in the measured vectors.
         masks : `numpy.ndarray` of shape ``(M, N)``
             Non-zero entries should be masked in the fit. If a boolean array,
             we'll mask entries where this is ``True``.
@@ -121,29 +153,77 @@ class FitFocalPlaneTask(Task):
             Function fit to the data.
         """
         centers = pfsConfig.extractCenters(fiberIdList)
-        return self.fit(wavelength, vectors, errors, masks, centers)
+        return self.fit(wavelength, vectors, variances, masks, centers)
 
-    def fit(self, wavelength, vectors, errors, masks, centers):
-        """Fit a vector function over the focal plane
+    def fitImpl(self, wavelength, values, masks, variances):
+        """Implementation to fit a vector function over the focal plane
 
-        This implementation is a placeholder, as it simply averages the input
-        vectors instead of doing any real fitting or rejection of outliers, and
-        no attention is paid to the position of the fibers on the focal plane.
+        This implementation is a placeholder, as no attention is paid to the
+        position of the fibers on the focal plane. Essentially, this is a coadd.
 
         We assume that all interpolation and resampling has already been done,
         so all the inputs have a common wavelength scale.
 
         Parameters
         ----------
-        wavelength : `numpy.ndarray` of length ``N``
+        wavelength : `numpy.ndarray` of `float`, length ``N``
             Wavelength values, of length ``N``.
-        vectors : `numpy.ndarray` of shape ``(M, N)``
-            Measured vectors of length ``N`` for ``M`` positions.
-        errors : `numpy.ndarray` of shape ``(M, N)``
+        values : `numpy.ndarray` of `float`, shape ``(M, N)``
+            Measured values, of length ``N`` for ``M`` positions.
+        masks : `numpy.ndarray` of `bool`, shape ``(M, N)``
+            Array indicating entries that should be masked.
+        variances : `numpy.ndarray` of `float`, shape ``(M, N)``
             Errors in the measured vectors.
-        masks : `numpy.ndarray` of shape ``(M, N)``
-            Non-zero entries should be masked in the fit. If a boolean array,
-            we'll mask entries where this is ``True``.
+        centers : `numpy.ndarray` of shape ``(M, 2)``
+            Fiber centers.
+
+        Returns
+        -------
+        fit : `FocalPlaneFunction`
+            Function fit to the data.
+        """
+        numSamples, length = values.shape
+        coaddValues = np.zeros(length, dtype=float)
+        coaddMask = np.ones(length, dtype=bool)
+        coaddVariance = np.zeros(length, dtype=float)
+        sumWeights = np.zeros(length, dtype=float)
+
+        for ii in range(numSamples):
+            weight = np.zeros(length, dtype=float)
+            with np.errstate(invalid="ignore", divide="ignore"):
+                good = ~masks[ii] & (variances[ii] > 0)
+                weight[good] = 1.0/variances[ii][good]
+                coaddValues[good] += values[ii][good]*weight[good]
+                coaddMask[good] = False
+                sumWeights += weight
+
+        good = sumWeights > 0
+        coaddValues[good] /= sumWeights[good]
+        coaddVariance[good] = 1.0/sumWeights[good]
+        coaddVariance[~good] = np.inf
+        coaddMask = ~good
+
+        return FocalPlaneFunction(wavelength, coaddValues, coaddMask, coaddVariance)
+
+    def fit(self, wavelength, values, masks, variances, centers):
+        """Fit a vector function over the focal plane
+
+        This implementation is a placeholder, as no attention is paid to the
+        position of the fibers on the focal plane. Essentially, this is a coadd.
+
+        We assume that all interpolation and resampling has already been done,
+        so all the inputs have a common wavelength scale.
+
+        Parameters
+        ----------
+        wavelength : `numpy.ndarray` of `float`, length ``N``
+            Wavelength values, of length ``N``.
+        values : `numpy.ndarray` of `float`, shape ``(M, N)``
+            Measured values, of length ``N`` for ``M`` positions.
+        masks : `numpy.ndarray` of `bool`, shape ``(M, N)``
+            Array indicating entries that should be masked.
+        variances : `numpy.ndarray` of `float`, shape ``(M, N)``
+            Errors in the measured vectors.
         centers : `numpy.ndarray` of shape ``(M, 2)``
             Fiber centers.
 
@@ -153,19 +233,76 @@ class FitFocalPlaneTask(Task):
             Function fit to the data.
         """
         wavelength = np.array(wavelength)
-        vectors = np.array(vectors)
+        values = np.array(values)
         masks = np.array(masks)
-        assert vectors.shape == masks.shape
-        assert len(vectors) == len(centers)
-        numSamples, numElements = vectors.shape
+        variances = np.array(variances)
+        assert values.shape == masks.shape
+        assert len(values) == len(centers)
+        numSamples, length = values.shape
+        assert variances.shape == values.shape
+        assert wavelength.shape == (length,)
         assert centers.shape == (numSamples, 2)
-        good = (masks == 0)
-        average = np.zeros(numElements, dtype=vectors.dtype)
-        for ii in range(numElements):
-            vv = vectors[:, ii]
-            gg = good[:, ii]
-            average[ii] = np.average(vv[gg]) if np.any(gg) else 0.0
-        return FocalPlaneFunction(wavelength, average)
+
+        rejected = np.zeros((numSamples, length), dtype=bool)
+        for ii in range(self.config.rejIterations):
+            func = self.fitImpl(wavelength, values, masks | rejected, variances)
+            with np.errstate(invalid="ignore", divide="ignore"):
+                resid = (values - func.value[np.newaxis, :])/np.sqrt(variances)
+                newRejected = ~rejected & ~masks & ~func.mask & (np.abs(resid) > self.config.rejThreshold)
+            if self.debugInfo.plot:
+                self.plot(wavelength, values, masks | rejected, func, f"Iteration {ii}", newRejected)
+            if not np.any(newRejected):
+                break
+            rejected |= newRejected
+        else:
+            func = self.fitImpl(wavelength, values, masks | rejected, variances)
+            resid = (values - func.value[np.newaxis, :])/np.sqrt(variances)
+            if self.debugInfo.plot:
+                self.plot(wavelength, values, masks | rejected, func, f"Final")
+
+        good = ~(rejected | masks)
+        chi2 = np.sum(resid[good]**2)
+        self.log.info("Fit focal plane function: "
+                      "chi^2=%f length=%d/%d numSamples=%d numGood=%d numRejected=%d",
+                      chi2, (~func.mask).sum(), length, numSamples, good.sum(), rejected.sum())
+        return func
+
+    def plot(self, wavelength, values, masks, func, title, rejected=None):
+        """Plot the input and fit values
+
+        Parameters
+        ----------
+        wavelength : `numpy.ndarray` of `float`, shape ``(M,)``
+            Common wavelength array.
+        values : `numpy.ndarray` of `float`, shape ``(N, M)``
+            Measured values for each sample+wavelength.
+        masks : `numpy.ndarray` of `bool`, shape ``(N, M)``
+            Boolean flag indicating whether a point should be masked.
+        func : `FocalPlaneFunction`
+            Fit result.
+        title : `str`
+            Title to use for the plot.
+        rejected : `numpy.ndarray` of `bool`, shape ``(N, M)``; optional
+            Boolean flag indicating whether a point has been rejected.
+        """
+        import matplotlib.pyplot as plt
+        num = len(values)
+        for ii in range(num):
+            plt.plot(wavelength, values[ii], "k-")
+            mm = masks[ii]
+            if np.any(mm):
+                plt.plot(wavelength[mm], values[ii][mm], "kx")
+            if rejected is not None:
+                rej = rejected[ii]
+                if np.any(rej):
+                    plt.plot(wavelength[rej], values[ii][rej], "rx")
+        plt.plot(func.wavelength, func.value, "b-")
+        if np.any(func.mask):
+            plt.plot(func.wavelength[func.mask], func.value[func.mask], "bx")
+        plt.xlabel("Wavelength (nm)")
+        plt.ylabel("Value")
+        plt.title(title)
+        plt.show()
 
     def apply(self, func, wavelength, fiberIdList, pfsConfig):
         """Apply the fit to fibers
@@ -184,8 +321,10 @@ class FitFocalPlaneTask(Task):
 
         Returns
         -------
-        result : `numpy.ndarray` of shape ``(M, N)``
-            Function fit to the data.
+        values : list (length ``N``) of `numpy.ndarray` of shape ``(M)``
+            Vector function evaluated at each position.
+        variances : list (length ``N``) of `numpy.ndarray` of shape ``(M)``
+            Variances for each position.
         """
         if len(wavelength.shape) == 1:
             wavelength = np.array([wavelength]*len(fiberIdList))
