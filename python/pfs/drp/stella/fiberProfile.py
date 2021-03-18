FiberProfile: fix normalisation when making traces
Before normalisation, the traces do not sum across rows to unity (the
higher the slope, the higher the departure from unit sum). When no
normalisation was specified, no normalisation was performed. When a
normalisation was provided, we multiplied by the desired normalisation
divided by the sum across the trace. Hence there was an inconsistency
between the two cases (normalisation vs no normalisation). Fixed to always
divide through by the sum across the trace.

python/pfs/drp/stella/fiberProfile.py
@@ -308,10 +308,11 @@ class FiberProfile:
             image.image.array[yy, xImg] = nextProfile*nextWeight[yy] + prevProfile*prevWeight[yy]
 
         # Set normalisation to what is desired
+        scale = 1.0/image.image.array.sum(axis=1)
         if self.norm is not None:
-            scale = (self.norm/image.image.array.sum(axis=1))[:, np.newaxis]
-            image.image.array *= scale
-            image.variance.array *= scale**2
+            scale *= self.norm
+        image.image.array *= scale[:, np.newaxis]
+        image.variance.array *= scale[:, np.newaxis]**2
 
         # Deselect bad pixels and bad rows
         ftBitMask = 2**image.mask.addMaskPlane("FIBERTRACE")