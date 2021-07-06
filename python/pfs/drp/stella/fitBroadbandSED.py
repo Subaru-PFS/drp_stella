0f9572f0c5b85483dc00e04d45070765c2c8e8b0
Sogo Mineo <31459905+SogoMineo@users.noreply.github.com>
2021-07-06 11:13:05 +0900

    Added fitBroadbandSED.py
    FitBroadbandSEDTask is a task to derive a probability distribution
    function from broad-band SED fitting of a standard star.
    The original program was provided by Takuji Yamashita.
    
    Requires PIPE2D-852 upgrade to drp_pfs_data.

---
+++ python/pfs/drp/stella/fitBroadbandSED.py
@@ -0,0 +1,167 @@
+from pfs.datamodel.pfsConfig import TargetType
+
+from lsst.pex.config import Config, Field, DictField
+from lsst.pipe.base import Task
+from lsst.utils import getPackageDir
+
+import numpy
+import numpy.lib.recfunctions
+import astropy.io.fits
+
+import os
+
+
+class FitBroadbandSEDConfig(Config):
+    """Configuration for FitBroadbandSEDTask
+    """
+
+    fluxLibraryPath = Field(
+        dtype=str,
+        default=os.path.join(getPackageDir("drp_pfs_data"), "fluxCalibration",
+                             "syntheticFlux_withInterp_ps_HSCPS1GaiaSDSS.fits"),
+        doc="Synthetic photometry table"
+    )
+
+    filterMappings = DictField(
+        keytype=str, itemtype=str,
+        default={
+            "g": "HSCg", "r": "HSCr", "r2": "HSCr2", "i": "HSCi", "i2": "HSCi2",
+            "z": "HSCz", "y": "HSCy"
+        },
+        doc="Conversion table from pfsConfig's filter names to those used by `fluxLibrary`"
+    )
+
+
+class FitBroadbandSEDTask(Task):
+    """Fit an observed SED with synthetic model SEDs
+    """
+
+    ConfigClass = FitBroadbandSEDConfig
+    _DefaultName = "fitBroadbandSED"
+
+    def runDataRef(self, dataRef):
+        """For each spectrum,
+        calculate probabilities of model SEDs matching the spectrum.
+
+        Only spectra for which ``targetType == TargetType.FLUXSTD``
+        are processed.
+
+        Parameters
+        ----------
+        dataRef : `lsst.daf.persistence.ButlerDataRef`
+            Data reference for merged spectrum.
+
+        Returns
+        -------
+        broadbandPDF : `list` of `numpy.array` of `float`
+            ``broadbandPDF[iSpectrum][iSED]`` is the normalized probability
+            of the SED ``iSED`` matching the spectrum ``iSpectrum``.
+            If the targetType of ``iSpectrum`` is not ``TargetType.FLUXSTD``,
+            ``broadbandPDF[iSpectrum]`` is None.
+        """
+        pfsConfig = dataRef.get("pfsConfig")
+        return self.run(pfsConfig)
+
+    def run(self, pfsConfig):
+        """For each spectrum,
+        calculate probabilities of model SEDs matching the spectrum.
+
+        Only spectra for which ``targetType == TargetType.FLUXSTD``
+        are processed, and included in the returned list.
+
+        Parameters
+        ----------
+        pfsConfig : `pfs.datamodel.pfsConfig.PfsConfig`
+            Top-end configuration, for identifying sky fibers.
+
+        Returns
+        -------
+        broadbandPDF : `list` of `numpy.array` of `float`
+            ``broadbandPDF[iSpectrum][iSED]`` is the normalized probability
+            of the SED ``iSED`` matching the spectrum ``iSpectrum``.
+            If the targetType of ``iSpectrum`` is not ``TargetType.FLUXSTD``,
+            ``broadbandPDF[iSpectrum]`` is None.
+        """
+        pdfs = []
+        for targetType, filterNames, fiberFlux, fiberFluxErr \
+                in zip(pfsConfig.targetType, pfsConfig.filterNames,
+                       pfsConfig.fiberFlux, pfsConfig.fiberFluxErr):
+            if targetType == TargetType.FLUXSTD:
+                pdfs.append(self.getProbabilities(filterNames, fiberFlux, fiberFluxErr))
+            else:
+                pdfs.append(None)
+
+        return pdfs
+
+    def getProbabilities(self, filterNames, fiberFlux, fiberFluxErr):
+        """Calculate a chi square and a probability for each model SED.
+
+        Parameters
+        ----------
+        filterNames : `list` of `str`
+            List of filters used to measure the fiber fluxes for each filter.
+            e.g. ``["g", "i"]``.
+        fiberFlux : `list` of `float`
+            Array of fiber fluxes for each fiber, in [nJy].
+        fiberFluxErr : `list` of `float`
+            Array of fiber flux errors for each fiber in [nJy].
+
+        Returns
+        -------
+        broadbandPDF : `numpy.array` of `float`
+            Array of normalized probabilities of the SED fitting.
+        """
+        if not (len(filterNames) == len(fiberFlux) == len(fiberFluxErr)):
+            raise ValueError("Lengths of arguments must be equal.")
+        if not filterNames:
+            nSEDs = len(self.fluxLibrary)
+            return numpy.full(shape=(nSEDs,), fill_value=1.0/nSEDs, dtype=float)
+
+        # Convert filter names.
+        filterNames = [self.config.filterMappings.get(f, f) for f in filterNames]
+
+        # Note: fluxLibrary.shape == (nSEDs, nBands)
+        fluxLibrary = numpy.lib.recfunctions.structured_to_unstructured(
+            self.fluxLibrary[filterNames], dtype=float
+        )
+
+        # Note: observedFluxes.shape == (1, nBands)
+        observedFluxes = numpy.asarray(fiberFlux, dtype=float).reshape(1, -1)
+        # Note: observedNoises.shape == (1, nBands)
+        observedNoises = numpy.asarray(fiberFluxErr, dtype=float).reshape(1, -1)
+        # Note: numer.shape == (nSEDs, 1)
+        numer = numpy.sum(fluxLibrary * (observedFluxes / (observedNoises**2)), axis=1, keepdims=True)
+        # Note: denom.shape == (nSEDs, 1)
+        denom = numpy.sum((fluxLibrary / observedNoises)**2, axis=1, keepdims=True)
+        # Note: alpha.shape == (nSEDs, 1)
+        alpha = numer / denom
+
+        # Compute chi square
+        # Note: chisq.shape == (nSEDs,)
+        chisq = numpy.sum(((observedFluxes - alpha*fluxLibrary)/observedNoises)**2, axis=1)
+
+        # Convert a list of chi squares to a probability distribution
+        delta_chisq = chisq - numpy.min(chisq)
+        prob = numpy.exp(delta_chisq / (-2.))
+        prob_norm = prob / numpy.sum(prob)
+
+        return prob_norm
+
+    @property
+    def fluxLibrary(self):
+        """Table of the synthetic model SEDs
+
+        Returns
+        -------
+        fluxLibrary : `numpy.array`
+            A structured array,
+            whose columns are SED parameters and various fluxes.
+        """
+        table = getattr(self, "_fluxLibrary", None)
+        if table is None:
+            # We convert FITS_rec back to numpy's structured array
+            # because FITS_rec cannot be indexed with multiple column names.
+            table = self._fluxLibrary = numpy.asarray(
+                astropy.io.fits.open(self.config.fluxLibraryPath)[1].data
+            )
+        return table
---
+++ tests/test_fitBroadbandSED.py
@@ -0,0 +1,107 @@
+import lsst.utils.tests
+
+from pfs.drp.stella.fitBroadbandSED import FitBroadbandSEDConfig, FitBroadbandSEDTask
+from pfs.drp.stella.tests import runTests
+from pfs.datamodel.pfsConfig import FiberStatus, PfsConfig, TargetType
+
+import numpy
+
+
+class FitBroadbandSEDTestCase(lsst.utils.tests.TestCase):
+    def setUp(self):
+        self.fitBroadbandSED = FitBroadbandSEDTask(config=FitBroadbandSEDConfig())
+
+    def testRun(self):
+        """Test run() method
+        """
+        random = numpy.random.default_rng(0xfeeda900d5eed)
+        filters = ["g", "r", "i", "z", "y"]
+        filters = [self.fitBroadbandSED.config.filterMappings.get(f, f) for f in filters]
+
+        trueSEDIndices = numpy.arange(len(self.fitBroadbandSED.fluxLibrary))[::5]
+        nFibers = len(trueSEDIndices)
+
+        filterNames = []
+        fiberFlux = []
+        fiberFluxErr = []
+        for record in self.fitBroadbandSED.fluxLibrary[trueSEDIndices]:
+            numFilters = random.integers(len(filters))
+            amplitude = random.uniform(0.1, 10)
+
+            random.shuffle(filters)
+            selectedFilters = filters[:numFilters]
+            selectedFluxes = amplitude * numpy.array([record[f] for f in selectedFilters], dtype=float)
+            selectedFluxErrs = numpy.abs(selectedFluxes) * random.uniform(0.01, 0.1, size=(numFilters,))
+
+            filterNames.append(selectedFilters)
+            fiberFlux.append(selectedFluxes)
+            fiberFluxErr.append(selectedFluxErrs)
+
+        allPatches = [
+            f"{x},{y}" for x, y in numpy.broadcast(numpy.arange(32).reshape(-1, 1),
+                                                   numpy.arange(32).reshape(1, -1))
+        ]
+        allPatches = numpy.array(allPatches, dtype=(str, 8))
+
+        allTargetTypes = numpy.array([int(x) for x in TargetType], dtype=numpy.int32)
+        allFiberStatuses = numpy.array([int(x) for x in FiberStatus], dtype=numpy.int32)
+
+        pfsConfig = PfsConfig(
+            pfsDesignId=1,
+            visit0=1,
+            raBoresight=1.0,
+            decBoresight=1.0,
+            posAng=1.0,
+            arms="brn",
+            fiberId=numpy.arange(nFibers, dtype=numpy.int32),
+            tract=random.integers(20000, size=(nFibers,), dtype=numpy.int32),
+            patch=random.choice(allPatches, size=(nFibers,)),
+            ra=random.uniform(360, size=(nFibers,)),
+            dec=random.uniform(-90, 90, size=(nFibers,)),
+            catId=random.integers(0x8000_0000, size=(nFibers,), dtype=numpy.int32),
+            objId=random.integers(0x8000_0000_0000_0000, size=(nFibers,), dtype=numpy.int64),
+            targetType=random.choice(allTargetTypes, size=(nFibers,)),
+            fiberStatus=random.choice(allFiberStatuses, size=(nFibers,)),
+            fiberFlux=fiberFlux,
+            psfFlux=fiberFlux,
+            totalFlux=fiberFlux,
+            fiberFluxErr=fiberFluxErr,
+            psfFluxErr=fiberFluxErr,
+            totalFluxErr=fiberFluxErr,
+            filterNames=filterNames,
+            pfiCenter=random.uniform(1000, size=(nFibers, 2)),
+            pfiNominal=random.uniform(1000, size=(nFibers, 2)),
+            guideStars=None,
+        )
+
+        pdfs = self.fitBroadbandSED.run(pfsConfig)
+
+        self.assertEqual(len(pdfs), nFibers)
+
+        for trueSED, targetType, fiberFlux, pdf \
+                in zip(trueSEDIndices, pfsConfig.targetType, pfsConfig.fiberFlux, pdfs):
+            if TargetType(targetType) == TargetType.FLUXSTD:
+                if len(fiberFlux) >= 3:
+                    # If three or more bands are available,
+                    # the true SED must be preferred the best.
+                    self.assertEqual(trueSED, numpy.argmax(pdf))
+                elif len(fiberFlux) >= 2:
+                    # If only two bands are available,
+                    # there may be two or more SEDs
+                    # whose probabilities tie for first place.
+                    # Still, the true SED must be one of them.
+                    self.assertFloatsAlmostEqual(pdf[trueSED] / numpy.max(pdf), 1)
+                else:
+                    # If no flux is available, or only one flux is available,
+                    # FitBroadbandSEDTask must not prefer any SED to the others.
+                    self.assertFloatsAlmostEqual(pdf, pdf[0])
+            else:
+                self.assertTrue(pdf is None)
+
+
+def setup_module(module):
+    lsst.utils.tests.init()
+
+
+if __name__ == "__main__":
+    runTests(globals())
