a8365bfeffb987465abc31b0686fe36608b26041
Paul Price <price@astro.princeton.edu>
2021-08-20 14:56:58 -0400

    refactor sky subtraction, flux calibration, mergeArms, coaddSpectra
    Sky subtraction and flux calibration are users of FitFocalPlaneTask,
    which has recently been refactored. Much of the SubtractSky1dTask and
    MeasureFluxCalibrationTask functionality has mostly moved into the
    FitFocalPlaneTask (iteration and rejection), so they can be replaced by
    thin functions to apply the results, and a small bit of code above them
    that will
    
    This requires a bit of refactoring of mergeArms and coaddSpectra, since
    these are the main users of the results of sky1d and fluxCal. In
    particular, we no longer allow 1d sky subtraction to be performed on
    resampled images, since we have oversampled splines; this simplifies the
    code because coaddSpectra no longer has to know when to apply the 1d sky
    subtraction.

--- python/pfs/drp/stella/subtractSky1d.py
+++ python/pfs/drp/stella/subtractSky1d.py
@@ -1,247 +1,29 @@
 import numpy as np
 
-from lsst.pex.config import Config, Field, ConfigurableField, ListField
-from lsst.pipe.base import Task
+from pfs.datamodel import PfsConfig
+from pfs.datamodel import PfsFiberArraySet
+from .focalPlaneFunction import FocalPlaneFunction
 
-from pfs.datamodel.pfsConfig import TargetType
-from .fitFocalPlane import FitFocalPlaneTask
-
-import lsstDebug
+__all__ = ("subtractSky1d",)
 
 
-class SubtractSky1dConfig(Config):
-    """Configuration for SubtractSky1dTask"""
-    fit = ConfigurableField(target=FitFocalPlaneTask, doc="Fit over the focal plane")
-    mask = ListField(dtype=str, default=["NO_DATA", "SAT", "BAD_FLAT"],
-                     doc="Mask flags for rejection of observed")
-    minWavelength = Field(dtype=float, default=300, doc="Minimum wavelength for resampled spectra (nm)")
-    maxWavelength = Field(dtype=float, default=1300, doc="Maximum wavelength for resampled spectra (nm)")
-    deltaWavelength = Field(dtype=float, default=0.03, doc="Wavelength spacing for resampled spectra (nm)")
-    sysErr = Field(dtype=float, default=1.0e-4,
-                   doc=("Fraction of value to add to variance before fitting. This attempts to offset the "
-                        "loss of variance as covariance when we resample, the result of which is "
-                        "underestimated errors and excess rejection."))
+def subtractSky1d(spectra: PfsFiberArraySet, pfsConfig: PfsConfig, sky1d: FocalPlaneFunction) -> None:
+    """Subtract sky model from spectra
 
-
-class SubtractSky1dTask(Task):
-    """Subtraction of sky in the 1D spectra
-
-    This is a placeholder implementation that simply fits the sky spectra over
-    the focal plane. No attempt is made to deal with the (bright) sky lines
-    separately from the continuum.
+    Parameters
+    ----------
+    spectra : `PfsFiberArraySet`
+        Spectra from which to subtract sky model. The spectra are modified.
+    pfsConfig : `PfsConfig`
+        Top-end configuration.
+    sky1d : `FocalPlaneFunction`
+        Sky model.
     """
-    ConfigClass = SubtractSky1dConfig
-    _DefaultName = "subtractSky1d"
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.makeSubtask("fit")
-        self.debugInfo = lsstDebug.Info(__name__)
-
-    def run(self, spectraList, pfsConfig, lsfList):
-        """Measure and subtract the sky from the 1D spectra
-
-        Parameters
-        ----------
-        spectraList : iterable of `pfs.datamodel.PfsFiberArraySet`
-            List of spectra from which to subtract the sky.
-        pfsConfig : `pfs.datamodel.PfsConfig`
-            Configuration of the top-end, for identifying sky fibers.
-        lsfList : iterable of LSF (type TBD)
-            List of line-spread functions.
-
-        Returns
-        -------
-        sky1d : `pfs.drp.stella.FocalPlaneFunction`
-            1D sky model.
-        """
-        if not np.any(pfsConfig.targetType == TargetType.SKY):
-            raise RuntimeError("No sky fibers found")
-        if self.debugInfo.plotSkyFluxes:
-            self.plotSkyFibers(spectraList, pfsConfig, "Sky flux")
-        resampledList = self.resampleSpectra(spectraList, pfsConfig)  # resampled SKY spectra
-        sky1d = self.measureSky(resampledList, pfsConfig, lsfList)
-        for spectra, lsf in zip(spectraList, lsfList):
-            self.subtractSkySpectra(spectra, lsf, pfsConfig, sky1d)
-        if self.debugInfo.plotSkyResiduals:
-            self.plotSkyFibers(spectraList, pfsConfig, "Sky residuals")
-        return sky1d
-
-    def resampleSpectra(self, spectraList, pfsConfig):
-        """Resample the sky spectra to a common wavelength scale
-
-        Parameters
-        ----------
-        spectraList : iterable of `pfs.datamodel.PfsFiberArraySet`
-            Spectra to resample
-        pfsConfig : `pfs.datamodel.PfsConfig`
-            Configuration of the top-end, for identifying sky fibers.
-
-        Returns
-        -------
-        resampled : `list` of `pfs.datamodel.PfsFiberArraySet`
-            Resampled sky fiber spectra.
-        """
-        minWl = self.config.minWavelength
-        maxWl = self.config.maxWavelength
-        dWl = self.config.deltaWavelength
-        wavelength = minWl + dWl*np.arange(int((maxWl - minWl)/dWl), dtype=float)
-        index = pfsConfig.selectByTargetType(TargetType.SKY)
-        return [spectra.resample(wavelength, pfsConfig.fiberId[index]) for spectra in spectraList]
-
-    def measureSky(self, spectraList, pfsConfig, lsfList, skyFiberIndexes=None):
-        """Measure the 1D sky model
-
-        Parameters
-        ----------
-        spectraList : iterable of `pfs.datamodel.PfsFiberArraySet`
-            List of spectra (with common wavelengths) from which to measure
-            the sky.
-        pfsConfig : `pfs.datamodel.PfsConfig`
-            Configuration of the top-end, for identifying sky fibers.
-        lsfList : iterable of LSF (type TBD)
-            List of line-spread functions.
-        skyFiberIndexes : iterable of `int` or None
-            Indexes of sky fibres in spectraList[]
-
-        Returns
-        -------
-        sky1d : `pfs.drp.stella.FocalPlaneFunction`
-            1D sky model.
-        """
-        if skyFiberIndexes is None:                      # only sky fibres are provided
-            skyFiberIndexes = range(len(spectraList[0]))  # all of spectraList are the same length
-        wavelength = spectraList[0].wavelength[0]  # They're all resampled to a common wavelength scale
-        values = []
-        variances = []
-        masks = []
-        fiberId = []
-        for spectra, lsf in zip(spectraList, lsfList):
-            maskVal = spectra.flags.get(*self.config.mask)
-            for ii in skyFiberIndexes:
-                flux = spectra.flux[ii]/spectra.norm[ii]
-                var = spectra.covar[ii][0]/spectra.norm[ii]**2
-                values.append(flux)
-                variances.append(var + self.config.sysErr*flux)
-                masks.append((spectra.mask[ii] & maskVal) != 0)
-                fiberId.append(spectra.fiberId[ii])
-
-        return self.fit.run(wavelength, values, masks, variances, fiberId, pfsConfig)
-
-    def subtractSkySpectra(self, spectra, lsf, pfsConfig, sky1d):
-        """Subtract the 1D sky model from the spectra, in-place
-
-        Parameters
-        ----------
-        spectra : `pfs.datamodel.PfsFiberArraySet`
-            Spectra to have sky subtracted.
-        lsf : LSF (type TBD)
-            Line-spread function.
-        pfsConfig : `pfs.datamodel.PfsConfig`
-            Top-end configuration, for getting location of fibers.
-        sky1d : `pfs.drp.stella.FocalPlaneFunction`
-            1D sky model.
-        """
-        sky = self.fit.apply(sky1d, spectra.wavelength, spectra.fiberId, pfsConfig)
-        skyValues = sky.values*spectra.norm
-        skyVariances = sky.variances*spectra.norm**2
-        spectra.flux -= skyValues
-        spectra.sky += skyValues
-        bitmask = spectra.flags.add("BAD_SKY")
-        spectra.mask[np.array(sky.masks)] |= bitmask
-        spectra.covar[:, 0, :] += skyVariances
-
-    def subtractSkySpectrum(self, spectrum, lsf, fiberId, pfsConfig, sky1d):
-        """Subtract the 1D sky model from the spectrum, in-place
-
-        Parameters
-        ----------
-        spectrum : `pfs.datamodel.PfsFiberArray`
-            Spectrum to have sky subtracted.
-        lsf : LSF (type TBD)
-            Line-spread function.
-        fiberId : `int`
-            Fiber identifier.
-        pfsConfig : `pfs.datamodel.PfsConfig`
-            Top-end configuration, for getting location of fibers.
-        sky1d : `pfs.drp.stella.FocalPlaneFunction`
-            1D sky model.
-        """
-        sky = self.fit.apply(sky1d, spectrum.wavelength, [fiberId], pfsConfig)
-        spectrum.flux -= sky.values[0]
-        spectrum.sky += sky.values[0]
-        bitmask = spectrum.flags.add("BAD_SKY")
-        spectrum.mask[np.array(sky.masks[0])] |= bitmask
-        spectrum.covar[0] += sky.variances[0]
-
-    def estimateSkyFromMerged(self, merged, pfsConfig, lsfList):
-        """Measure and subtract the sky from the merged 1D spectra
-
-        Parameters
-        ----------
-        merged : `pfs.datamodel.PfsMerged`
-            List of merged spectra from which to subtract the sky,
-            including sky spectra
-        pfsConfig : `pfs.datamodel.PfsConfig`
-            Configuration of the top-end, for identifying sky fibers.
-        lsfList : iterable of LSF (type TBD)
-            List of line-spread functions.
-
-        Returns
-        -------
-        sky1d : `pfs.drp.stella.FocalPlaneFunction`
-            1D sky model.
-        """
-        if not np.any(pfsConfig.targetType == TargetType.SKY):
-            raise RuntimeError("No sky fibers found")
-
-        if self.debugInfo.plotSkyFluxes:
-            self.plotSkyFibers([merged], pfsConfig, "Sky flux")
-
-        sky1d = self.measureSky([merged], pfsConfig, [lsfList],
-                                pfsConfig.selectByTargetType(TargetType.SKY, merged.fiberId))
-
-        self.subtractSkySpectra(merged, lsfList, pfsConfig, sky1d)
-
-        if self.debugInfo.plotSkyResiduals:
-            self.plotSkyFibers([merged], pfsConfig, "Sky residuals")
-
-        return sky1d
-
-    def plotSkyFibers(self, spectraList, pfsConfig, title):
-        """Plot spectra from sky fibers
-
-        The spectra from different fibers are shown as different colors.
-        Masked points are drawn as dots.
-
-        Parameters
-        ----------
-        spectraList : iterable of `PfsArm`
-            Extracted spectra from spectrograph arms for a single exposure.
-        pfsConfig : `pfs.datamodel.PfsConfig`
-            Top-end configuration, for identifying sky fibers.
-        title : `str`
-            Title for plot.
-        """
-        import matplotlib.pyplot as plt
-        import matplotlib.cm
-
-        indices = pfsConfig.selectByTargetType(TargetType.SKY)
-        fiberId = dict(zip(pfsConfig.fiberId[indices],
-                           matplotlib.cm.rainbow(np.linspace(0, 1, len(indices)))))
-
-        figure, axes = plt.subplots()
-        for spectra in spectraList:
-            for ii, ff in enumerate(spectra.fiberId):
-                if ff not in fiberId:
-                    continue
-                axes.plot(spectra.wavelength[ii], spectra.flux[ii], ls="solid", color=fiberId[ff])
-                bad = (spectra.mask[ii] & spectra.flags.get(*self.config.mask)) != 0
-                if np.any(bad):
-                    axes.plot(spectra.wavelength[ii][bad], spectra.flux[ii][bad], ".", color=fiberId[ff])
-
-        axes.set_xlabel("Wavelength (nm)")
-        axes.set_ylabel("Flux")
-        axes.set_title(title)
-        figure.show()
-        input("Hit ENTER to continue... ")
+    sky = sky1d(spectra.wavelength, pfsConfig.select(fiberId=spectra.fiberId))
+    skyValues = sky.values*spectra.norm
+    skyVariances = sky.variances*spectra.norm**2
+    spectra.flux -= skyValues
+    spectra.sky += skyValues
+    bitmask = spectra.flags.add("BAD_SKY")
+    spectra.mask[np.array(sky.masks)] |= bitmask
+    spectra.covar[:, 0, :] += skyVariances
