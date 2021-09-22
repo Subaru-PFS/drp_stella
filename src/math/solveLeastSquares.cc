061af890fef3dea14c11f584827d77780261d68b
Paul Price <price@astro.princeton.edu>
2021-09-22 18:19:04 -0400

    add pfs::drp::stella::math::solveLeastSquaresDesign
    to solve a least-squares equation specified by a design matrix.

---
+++ src/math/solveLeastSquares.cc
@@ -0,0 +1,41 @@
+#include "ndarray.h"
+#include "ndarray/eigen.h"
+
+#include "lsst/afw/math/LeastSquares.h"
+
+#include "pfs/drp/stella/math/solveLeastSquares.h"
+
+namespace pfs {
+namespace drp {
+namespace stella {
+namespace math {
+
+
+ndarray::Array<double, 1, 1> solveLeastSquaresDesign(
+    ndarray::Array<double, 2, 1> const& design,
+    ndarray::Array<double, 1, 1> const& meas,
+    ndarray::Array<double, 1, 1> const& err
+) {
+    std::size_t const numValues = design.getShape()[0];
+    std::size_t const numParams = design.getShape()[1];
+    assert(meas.size() == numValues);
+    assert(err.size() == numValues);
+
+    ndarray::Array<double, 2, 1> designNorm = ndarray::copy(design);
+    for (std::size_t ii = 0; ii < numParams; ++ii) {
+        designNorm[ndarray::view()(ii)] /= err;
+    }
+    ndarray::Array<double, 1, 1> measNorm = ndarray::copy(meas);
+    measNorm.deep() /= err;
+
+    auto const dd = asEigenMatrix(designNorm);
+
+    ndarray::Array<double, 2, 1> fisher = ndarray::allocate(numParams, numParams);
+    ndarray::Array<double, 1, 1> rhs = ndarray::allocate(numParams);
+    asEigenMatrix(fisher) = dd.transpose()*dd;
+    asEigenMatrix(rhs) = dd.transpose()*asEigenMatrix(measNorm);
+    return copy(lsst::afw::math::LeastSquares::fromNormalEquations(fisher, rhs).getSolution());
+}
+
+
+}}}}  // namespace pfs::drp::stella::math
