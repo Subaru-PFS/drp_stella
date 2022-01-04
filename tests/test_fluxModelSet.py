30b09fa8117093802ccde194a0fb21a48494e664
Sogo Mineo <31459905+SogoMineo@users.noreply.github.com>
2022-01-04 13:51:12 +0900

    Add class FluxModelSet
    FluxModelSet is a class that helps to deal with spectra
    in fluxmodeldata package.

---
+++ tests/test_fluxModelSet.py
@@ -0,0 +1,36 @@
+import lsst.utils
+import lsst.utils.tests
+from pfs.datamodel.pfsSimpleSpectrum import PfsSimpleSpectrum
+from pfs.drp.stella.fluxModelSet import FluxModelSet
+from pfs.drp.stella.tests import runTests
+
+import unittest
+
+try:
+    dataDir = lsst.utils.getPackageDir("fluxmodeldata")
+except LookupError:
+    dataDir = None
+
+
+@unittest.skipIf(dataDir is None, "fluxmodeldata not setup")
+class FluxModelSetTestCase(lsst.utils.tests.TestCase):
+    def setUp(self):
+        self.fluxModelSet = FluxModelSet(dataDir)
+
+    def testGetSpectrum(self):
+        """Test getSpectrum()
+        """
+        parameters = self.fluxModelSet.parameters
+        param = parameters[len(parameters) // 2]
+        spectrum = self.fluxModelSet.getSpectrum(
+            teff=param["teff"], logg=param["logg"], m=param["m"], alpha=param["alpha"],
+        )
+        self.assertIsInstance(spectrum, PfsSimpleSpectrum)
+
+
+def setup_module(module):
+    lsst.utils.tests.init()
+
+
+if __name__ == "__main__":
+    runTests(globals())
