96841117bc4c0325d0600fa9639a05c0937064f5
Paul Price <price@astro.princeton.edu>
2022-02-10 20:46:46 -0500

    constructFiberFlat: revise profileRadius
    to something that's appropriate for full fiber density.
    Using the default (5?) means that every fiber profile
    includes parts from its neighbours.

--- python/pfs/drp/stella/constructFiberFlatTask.py
+++ python/pfs/drp/stella/constructFiberFlatTask.py
@@ -42,6 +42,7 @@ class ConstructFiberFlatTask(SpectralCalibTask):
         config.isr.doFlat = False
         config.isr.doFringe = False
         config.profiles.doBlindFind = True  # Because we've dithered the fiber positions
+        config.profiles.profileRadius = 2  # Full fiber density, so can't go out very wide
 
     def combine(self, cache, struct, outputId):
         """Combine multiple exposures of a particular CCD and write the output
