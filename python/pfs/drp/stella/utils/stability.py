2b512ca12a698d0bfa6f7ee1be0f5cb03377c61f
Hassan Siddiqui <hassans@astro.princeton.edu>
2022-02-16 08:02:06 -0500

    WIP: added test wavelength value to accommodate new getDispersion signature

--- python/pfs/drp/stella/utils/stability.py
+++ python/pfs/drp/stella/utils/stability.py
@@ -97,7 +97,7 @@ def plotArcResiduals(als,
         if detectorMap is None:
             raise RuntimeError("You must provide a DetectorMap if usePixels is True")
 
-        dispersion = detectorMap.getDispersion(als.fiberId[len(als.fiberId)//2])
+        dispersion = detectorMap.getDispersion(fiberId=als.fiberId[len(als.fiberId)//2], wavelength=700.0)
 
     ll = (als.flag == False)   # centroider succeeded # noqa E712: als.flag is a numpy array
     with np.testing.suppress_warnings() as suppress:
@@ -190,7 +190,7 @@ def plotArcResiduals2D(als, detectorMap, title="", fitType="mean",
     """
     arrowSize: characteristic arrow length in pixels
     """
-    dy = (als.lam - als.wavelength)/detectorMap.getDispersion(als.fiberId[len(als.fiberId)//2])
+    dy = (als.lam - als.wavelength)/detectorMap.getDispersion(fiberId=als.fiberId[len(als.fiberId)//2], wavelength=700.0)
     dx = als.tracePos - als.x
 
     with np.testing.suppress_warnings() as suppress:
@@ -199,7 +199,7 @@ def plotArcResiduals2D(als, detectorMap, title="", fitType="mean",
         ll = np.logical_and(ll, np.hypot(als.xErr, als.yErr) < maxCentroidErr)
         ll = np.logical_and(ll, np.hypot(dx, dy) < maxDetectorMapError)
 
-    dy = (als.lam - als.wavelength)/detectorMap.getDispersion(als.fiberId[len(als.fiberId)//2])
+    dy = (als.lam - als.wavelength)/detectorMap.getDispersion(als.fiberId[len(als.fiberId)//2], wavelength=700)
     dx = als.tracePos - als.x
 
     for dz in [dx, dy]:
@@ -372,7 +372,7 @@ class PlotArcLines:
             return f"{'%(visit)d %(arm)s%(spectrograph)d' % dataId}   " \
                 f"{md['DATE-OBS']}T{md['UT'][:-4]}   {fitName}"
 
-        dy = (als.lam - als.wavelength)/detectorMap.getDispersion(als.fiberId[len(als.fiberId)//2])
+        dy = (als.lam - als.wavelength)/detectorMap.getDispersion(als.fiberId[len(als.fiberId)//2], wavelength=700.0)
         dx = als.tracePos - als.x
 
         for i in range(2):
