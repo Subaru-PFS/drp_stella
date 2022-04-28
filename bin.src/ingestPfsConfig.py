903b27cd0d600c8cdb8f487272365df685e50d9a
Paul Price <price@astro.princeton.edu>
2022-04-28 16:12:12 -0400

    add Gen3 utilities
    In the Gen3 PipelineTask.runQuantum method, users are provided with
    data references for each of the connections, but connections with similar
    or even the same dimensions there is no guarantee that the data references
    are aligned (i.e., they cannot be assumed to be ordered in the same way),
    so simple zip iteration doesn't do what one would naively expect. This
    contribution includes functions zipDatasetRefs and readDatasetRefs that
    provides the alignment.
    
    This also includes functions to ingest a pfsConfig into the datastore.
    We could use "butler ingest-files", but this also reads the pfsConfig to
    provide a list of tracts and patches for the registry. This is important
    for the link between individual exposures and groups of coadded spectra.

---
+++ bin.src/ingestPfsConfig.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from argparse import ArgumentParser
+from pfs.drp.stella.gen3 import ingestPfsConfig
+
+
+def main():
+    """Run ingestPfsConfig"""
+    parser = ArgumentParser(description="Ingest a pfsConfig file into a gen3 datastore")
+    parser.add_argument("repo", help="URI for datastore repository")
+    parser.add_argument("instrument", help="Instrument name or fully-qualified instrument class")
+    parser.add_argument("run", help="Run into which to ingest files")
+    parser.add_argument("skymap", help="Name of skymap system")
+    parser.add_argument("path", nargs="+", help="Path/glob for pfsConfig files")
+    parser.add_argument(
+        "--transfer",
+        default="auto",
+        choices=("auto", "move", "copy", "direct", "split", "link", "hardlink", "relsymlink", "symlink"),
+        help="Mode for transferring files into the datastore",
+    )
+    parser.add_argument(
+        "--no-update", default=False, action="store_true", help="Don't allow updating of the registry"
+    )
+
+    args = parser.parse_args()
+    ingestPfsConfig(
+        args.repo, args.instrument, args.run, args.skymap, args.path, args.transfer, not args.no_update
+    )
+
+
+if __name__ == "__main__":
+    main()
