be2467a63630453303c7eaa458fd9dbd0335c1a4
Paul Price <price@astro.princeton.edu>
2023-06-13 15:24:31 -0400

    astropyFix: expand removal of thread locks
    There are locks used in the astropy units module, which I'm
    removing because I've had trouble with deadlocks again
    recently.

--- python/pfs/drp/stella/astropyFix.py
+++ python/pfs/drp/stella/astropyFix.py
@@ -23,6 +23,7 @@ burn Gen2 with fire.
 """
 
 import astropy.utils.decorators
+import astropy.units.quantity_helper.converters
 
 _NotFound = astropy.utils.decorators._NotFound
 
@@ -99,6 +100,109 @@ class lazyproperty(property):  # noqa N801: name follows astropy
         return type(self), (self.fget, self.fset, self.fdel, self.doc)
 
 
+class UfuncHelpers(dict):
+    """Registry of unit conversion functions to help ufunc evaluation.
+
+    Based on dict for quick access, but with a missing method to load
+    helpers for additional modules such as scipy.special and erfa.
+
+    Such modules should be registered using ``register_module``.
+    """
+
+    def __init__(self, *args, **kwargs):
+        self.modules = {}
+        self.UNSUPPORTED = set()  # Upper-case for backwards compatibility
+        super().__init__(*args, **kwargs)
+
+    @classmethod
+    def copy(cls, other):
+        """Copy the helpers from another UfuncHelpers instance."""
+        new = cls()
+        for key, val in other.items():
+            new[key] = val
+        new.modules = other.modules.copy()
+        new.UNSUPPORTED = other.UNSUPPORTED.copy()
+        return new
+
+    def register_module(self, module, names, importer):
+        """Register (but do not import) a set of ufunc helpers.
+
+        Parameters
+        ----------
+        module : str
+            Name of the module with the ufuncs (e.g., 'scipy.special').
+        names : iterable of str
+            Names of the module ufuncs for which helpers are available.
+        importer : callable
+            Function that imports the ufuncs and returns a dict of helpers
+            keyed by those ufuncs.  If the value is `None`, the ufunc is
+            explicitly *not* supported.
+        """
+        self.modules[module] = {"names": names, "importer": importer}
+
+    def import_module(self, module):
+        """Import the helpers from the given module using its helper function.
+
+        Parameters
+        ----------
+        module : str
+            Name of the module. Has to have been registered beforehand.
+        """
+        module_info = self.modules.pop(module)
+        self.update(module_info["importer"]())
+
+    def __missing__(self, ufunc):
+        """Called if a ufunc is not found.
+
+        Check if the ufunc is in any of the available modules, and, if so,
+        import the helpers for that module.
+        """
+        if ufunc in self.UNSUPPORTED:
+            raise TypeError(f"Cannot use ufunc '{ufunc.__name__}' with quantities")
+
+        for module, module_info in list(self.modules.items()):
+            if ufunc.__name__ in module_info["names"]:
+                # A ufunc with the same name is supported by this module.
+                # Of course, this doesn't necessarily mean it is the
+                # right module. So, we try let the importer do its work.
+                # If it fails (e.g., for `scipy.special`), then that's
+                # fine, just raise the TypeError.  If it succeeds, but
+                # the ufunc is not found, that is also fine: we will
+                # enter __missing__ again and either find another
+                # module or get the TypeError there.
+                try:
+                    self.import_module(module)
+                except ImportError:  # pragma: no cover
+                    pass
+                else:
+                    return self[ufunc]
+
+        raise TypeError(
+            f"unknown ufunc {ufunc.__name__}.  If you believe this ufunc "
+            "should be supported, please raise an issue on "
+            "https://github.com/astropy/astropy"
+        )
+
+    def __setitem__(self, key, value):
+        # Implementation note: in principle, we could just let `None`
+        # mean that something is not implemented, but this means an
+        # extra if clause for the output, slowing down the common
+        # path where a ufunc is supported.
+        if value is None:
+            self.UNSUPPORTED |= {key}
+            self.pop(key, None)
+        else:
+            super().__setitem__(key, value)
+            self.UNSUPPORTED -= {key}
+
+
 # Monkey-patch astropy
 astropy.utils.decorators.lazyproperty = lazyproperty
 astropy.utils.lazyproperty = lazyproperty
+
+UFUNC_HELPERS = UfuncHelpers.copy(
+    astropy.units.quantity_helper.converters.UFUNC_HELPERS
+)
+astropy.units.quantity_helper.converters.UfuncHelpers = UfuncHelpers
+astropy.units.quantity_helper.converters.UFUNC_HELPERS = UFUNC_HELPERS
+astropy.units.quantity_helper.UFUNC_HELPERS = UFUNC_HELPERS
