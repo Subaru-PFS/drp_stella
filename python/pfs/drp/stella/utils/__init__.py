b5e550e96df57798641f065584d9fa002dd62976
Robert Lupton the Good <rhl@astro.princeton.edu>
2024-09-22 20:35:49 -0300

    Reimplement/copy savgol_filter with support for error estimate
    Yes; I should push this upstream to scipy

--- python/pfs/drp/stella/utils/__init__.py
+++ python/pfs/drp/stella/utils/__init__.py
@@ -2,6 +2,7 @@ from .asinhNorm import *
 from .display import *
 from .fiberProfiles import *
 from .guiders import *
+from .math import *
 from .plotting import *
 from .sunss import *
 from .sysUtils import *
