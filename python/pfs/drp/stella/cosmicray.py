7cbfe9e7e544ce3e8f8148d09a1a6c767b3feeb5
Paul Price <price@astro.princeton.edu>
2024-10-30 11:19:39 -0400

    add CompareCosmicRayTask
    to identify cosmic rays from comparing multiple exposures.
    Adapted from an RHL notebook.
    
    Currently works on all visits provided to pipetask, but I
    intend that it will ultimately work on pre-defined groups
    of visits.

--- python/pfs/drp/stella/cosmicray.py
+++ python/pfs/drp/stella/cosmicray.py
@@ -1,15 +1,27 @@
-from typing import ClassVar, Type
+from typing import ClassVar, List, Type, TYPE_CHECKING
+
+import numpy as np
 
 from lsst.pipe.base import PipelineTask, PipelineTaskConfig, PipelineTaskConnections, Struct
 from lsst.pipe.base.connectionTypes import Output as OutputConnection
 from lsst.pipe.base.connectionTypes import Input as InputConnection
 from lsst.pipe.base import QuantumContext
 from lsst.pipe.base.connections import InputQuantizedConnection, OutputQuantizedConnection
-from lsst.pex.config import Config, ConfigurableField, Field
+from lsst.pex.config import Config, ConfigField, ConfigurableField, Field, makePropertySet
+
+from lsst.afw.detection import setMaskFromFootprintList
+from lsst.afw.image import ImageF, MaskedImageF
+from lsst.meas.algorithms import findCosmicRays, FindCosmicRaysConfig, GaussianPsfFactory
+from lsst.ip.isr.isrFunctions import growMasks
 
 from .repair import PfsRepairTask
 
-__all__ = ("CosmicRayTask",)
+if TYPE_CHECKING:
+    from lsst.afw.image import Exposure, MaskedImage, Mask
+    from lsst.afw.detection import Psf
+
+
+__all__ = ("CosmicRayTask", "CompareCosmicRayTask")
 
 
 class CosmicRayConnections(
@@ -74,3 +86,200 @@ class CosmicRayTask(PipelineTask):
             self.repair.run(exposure)
 
         return Struct(exposure=exposure)
+
+
+class CompareCosmicRayConnections(
+    PipelineTaskConnections,
+    dimensions=("instrument", "arm", "spectrograph"),  # XXX add visit_group
+):
+    """Connections for CompareCosmicRayTask"""
+    inputExposures = InputConnection(
+        name="postISRCCD",
+        doc="Exposure to repair",
+        storageClass="Exposure",
+        dimensions=("instrument", "visit", "arm", "spectrograph"),
+        multiple=True,
+    )
+    outputExposures = OutputConnection(
+        name="calexp",
+        doc="Repaired exposure",
+        storageClass="Exposure",
+        dimensions=("instrument", "visit", "arm", "spectrograph"),
+        multiple=True,
+    )
+
+
+class CompareCosmicRayConfig(PipelineTaskConfig, pipelineConnections=CompareCosmicRayConnections):
+    """Configuration for CompareCosmicRayTask"""
+    minVisitsMedian = Field(dtype=int, default=5, doc="Minimum number of visits to use median")
+    modelPsf = GaussianPsfFactory.makeField(doc="Model PSF")
+    cosmicray = ConfigField(dtype=FindCosmicRaysConfig, doc="Find cosmic rays")
+    grow = Field(dtype=int, default=3, doc="Radius to grow CRs")
+    doCosmicRay = Field(dtype=bool, default=True, doc="Remove cosmic rays?")
+    repair = ConfigurableField(target=PfsRepairTask, doc="Task to repair artifacts; used for single exposure")
+
+    def setDefaults(self):
+        super().setDefaults()
+        self.modelPsf.defaultFwhm = 4.0  # Not the real PSF FWHM, but this sets the CR finding sensitivity
+        self.cosmicray.nCrPixelMax = 100000000
+
+
+class CompareCosmicRayTask(PipelineTask):
+    """Perform cosmic-ray removal
+
+    We use comparison of multiple exposures of the same targets to identify
+    cosmic rays.
+    """
+    ConfigClass: ClassVar[Type[Config]] = CompareCosmicRayConfig
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.makeSubtask("repair")
+
+    def runQuantum(
+        self,
+        butler: QuantumContext,
+        inputRefs: InputQuantizedConnection,
+        outputRefs: OutputQuantizedConnection
+    ) -> None:
+        """Entry point for running the task under the Gen3 middleware"""
+        exposures = butler.get(inputRefs.inputExposures)
+        outputs = self.run(exposures)
+        butler.put(outputs.exposures, outputRefs.outputExposures)
+
+    def run(self, exposures: List["Exposure"]) -> Struct:
+        """Perform cosmic-ray removal
+
+        Parameters
+        ----------
+        exposures : `list` of `lsst.afw.image.Exposure`
+            Exposures to repair.
+
+        Returns
+        -------
+        result : `lsst.pipe.base.Struct`
+            Results of cosmic-ray removal.
+        """
+        result = Struct(exposures=exposures)
+        if len(exposures) == 0:
+            return result
+        if not self.config.doCosmicRay or exposures[0].getDetector().getName().startswith("n"):
+            return result
+
+        # Sinle exposure cosmic-ray removal: use the repair task
+        if len(exposures) == 1:
+            for exp in exposures:
+                self.repair.run(exp)
+            return result
+
+        # Multiple exposure cosmic-ray removal
+        clean = self.calculateCleanImage([exposure.getMaskedImage() for exposure in exposures])
+        result.mergeItems(clean, "image", "scales")
+        psf = self.config.modelPsf.apply()
+        for exp, scale in zip(exposures, clean.scales):
+            self.findCosmicRays(exp.getMaskedImage(), psf, clean.image, scale)
+        self.findOverlaps([exp.mask for exp in exposures])
+
+        return result
+
+    def calculateCleanImage(self, images: List["MaskedImage"]) -> ImageF:
+        """Calculate a cleaned image from a list of images
+
+        Parameters
+        ----------
+        images : `list` of `lsst.afw.image.MaskedImage`
+            List of images to clean.
+
+        Returns
+        -------
+        image : `lsst.afw.image.Image`
+            Cleaned image.
+        scales : `numpy.ndarray`
+            Scaling factor to multiple the cleaned image by to match the
+            input image.
+        """
+        num = len(images)
+        dims = images[0].getDimensions()
+        for image in images:
+            if image.getDimensions() != dims:
+                raise ValueError("Images must have the same dimensions")
+
+        fluxes = np.zeros(num, dtype=float)
+        stack = np.empty((num, dims.getY(), dims.getX()), dtype=np.float32)
+        for ii, image in enumerate(images):
+            fluxes[ii] = np.median(image.image.array)
+
+        fluxes /= np.mean(fluxes)
+
+        for ii, image in enumerate(images):
+            stack[ii] = image.image.array / fluxes[ii]
+
+        operation = np.nanmin if num < self.config.minVisitsMedian else np.nanmedian
+        clean = ImageF(dims)
+        clean.array[:] = operation(stack, axis=0)
+
+        return Struct(image=clean, scales=fluxes)
+
+    def findCosmicRays(self, image: "MaskedImage", psf: "Psf", clean: ImageF, scale: float):
+        """Find cosmic rays in an image
+
+        We subtracted the clean image and then find cosmic rays in the residual.
+
+        Parameters
+        ----------
+        image : `lsst.afw.image.MaskedImage`
+            Image to find cosmic rays in.
+        psf : `lsst.afw.detection.Psf`
+            PSF model.
+        clean : `lsst.afw.image.ImageF`
+            Cleaned image.
+        scale : `float`
+            Scaling factor to multiple the cleaned image by to match the
+            input image.
+        """
+        subtracted = MaskedImageF(image.image.clone(), image.mask, image.variance)
+        subtracted.image.array -= clean.array*scale
+
+        crBit = image.mask.getPlaneBitMask("CR")
+        image.mask &= ~crBit
+        cosmicrays = findCosmicRays(subtracted, psf, 0.0, makePropertySet(self.config.cosmicray), True)
+
+        num = 0
+        numPixels = 0
+        if cosmicrays is not None:
+            setMaskFromFootprintList(image.mask, cosmicrays, crBit)
+            num = len(cosmicrays)
+            numPixels = np.sum((image.mask.array & crBit) != 0)
+
+        self.log.info("Found %d cosmic rays (%d pixels)", num, numPixels)
+
+    def findOverlaps(self, masks: List["Mask"]):
+        """Find overlapping cosmic rays in a set of masks
+
+        The cleaned image will still include cosmic rays if they are present in
+        all the input images. To remove these, we find the pixels that are
+        near identified cosmic rays in all images.
+
+        Parameters
+        ----------
+        masks : `list` of `lsst.afw.image.Mask`
+            Masks to find overlapping cosmic rays in.
+        """
+        num = len(masks)
+        dims = masks[0].getDimensions()
+        for mask in masks:
+            if mask.getDimensions() != dims:
+                raise ValueError("Masks must have the same dimensions")
+
+        count = np.zeros((dims.getY(), dims.getX()), dtype=int)
+        for mm in masks:
+            mm = mm.clone()
+            crBit = mm.getPlaneBitMask("CR")
+            mm &= crBit
+            growMasks(mm, self.config.grow, "CR", "CR")
+            count[mm.array != 0] += 1
+
+        select = (count == num)
+        self.log.info("Found %d overlapping cosmic ray pixels", select.sum())
+        for mm in masks:
+            mm.array[select] |= mm.getPlaneBitMask("CR")
