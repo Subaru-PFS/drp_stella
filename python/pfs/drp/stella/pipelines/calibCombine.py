9f564631858a80d8f464ce9c776d783068bf3d8a
Paul Price <price@astro.princeton.edu>
2025-03-24 13:02:31 -0400

    calibs: work around DM-47920
    cp_pipe is writing HIERARCH keys including a period are
    triggering a bug in reading WCS. This bug was fixed in
    afw a couple of months after the 28.0.1 tag. I don't want
    to go to the trouble of figuring out how to patch afw,
    so I'm patching cp_pipe's CalibCombineTask so that it
    doesn't trigger the bug.

--- python/pfs/drp/stella/pipelines/calibCombine.py
+++ python/pfs/drp/stella/pipelines/calibCombine.py
@@ -1,3 +1,7 @@
+import numpy as np
+
+import lsst.afw.image as afwImage
+
 from lsst.pipe.base.connectionTypes import Input as InputConnection
 from lsst.pipe.base.connectionTypes import Output as OutputConnection
 from lsst.cp.pipe.cpCombine import CalibCombineConnections, CalibCombineConfig, CalibCombineTask
@@ -30,3 +34,34 @@ class PfsCalibCombineConfig(CalibCombineConfig, pipelineConnections=PfsCalibComb
 class PfsCalibCombineTask(CalibCombineTask):
     """Combine pre-processed exposures to create a proposed calibration"""
     ConfigClass = PfsCalibCombineConfig
+
+    def calibStats(self, exp, calibrationType):
+        """Measure bulk statistics for the calibration.
+
+        Override to work around DM-47920 (present in LSST 28). HIERARCH keys
+        with "." are triggering the bug, so we replace "." with "_".
+
+        Parameters
+        ----------
+        exp : `lsst.afw.image.Exposure`
+            Exposure to calculate statistics for.
+        calibrationType : `str`
+            Type of calibration to record in header.
+        """
+        metadata = exp.getMetadata()
+
+        noGoodPixelsBit = afwImage.Mask.getPlaneBitMask(self.config.noGoodPixelsMask)
+
+        # percentiles
+        for amp in exp.getDetector():
+            ampImage = exp[amp.getBBox()]
+            percentileValues = np.nanpercentile(ampImage.image.array,
+                                                self.config.distributionPercentiles)
+            for level, value in zip(self.config.distributionPercentiles, percentileValues):
+                key = f"LSST CALIB {calibrationType.upper()} {amp.getName()} DISTRIBUTION {level}-PCT"
+                key = key.replace(".", "_")
+                metadata[key] = value
+
+            bad = ((ampImage.mask.array & noGoodPixelsBit) > 0)
+            key = f"LSST CALIB {calibrationType.upper()} {amp.getName()} BADPIX-NUM"
+            metadata[key] = bad.sum()
