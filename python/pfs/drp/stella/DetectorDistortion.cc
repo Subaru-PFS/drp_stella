69864c01b62e68432764f708eec4ebd4b0db9834
Paul Price <price@astro.princeton.edu>
2025-04-08 12:48:51 -0400

    fix lsst::utils
    Which is now called lsst::cpputils.

--- python/pfs/drp/stella/DetectorDistortion.cc
+++ python/pfs/drp/stella/DetectorDistortion.cc
@@ -2,7 +2,7 @@
 #include <pybind11/stl.h>
 
 #include "ndarray/pybind11.h"
-#include "lsst/utils/python.h"
+#include "lsst/cpputils/python.h"
 #include "pfs/drp/stella/DetectorDistortion.h"
 #include "pfs/drp/stella/python/Distortion.h"
 
@@ -34,8 +34,8 @@ void declareDetectorDistortion(py::module & mod) {
     cls.def("getXDistortion", &Class::getXDistortion);
     cls.def("getYDistortion", &Class::getYDistortion);
     cls.def("getRightCcdCoefficients", &Class::getRightCcdCoefficients);
-    lsst::utils::python::addOutputOp(cls, "__str__");
-    lsst::utils::python::addOutputOp(cls, "__repr__");
+    lsst::cpputils::python::addOutputOp(cls, "__str__");
+    lsst::cpputils::python::addOutputOp(cls, "__repr__");
 }
 
 
