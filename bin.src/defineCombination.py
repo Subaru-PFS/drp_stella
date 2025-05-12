7f169e39fbb0fc3aaa1a8cc8c331324d8b35e8c1
Paul Price <price@astro.princeton.edu>
2025-05-12 16:32:04 -0400

    perform coadds over groups of objects within a catId
    pfsCoadds are getting too large, so we're subdividing a catId
    into groups of objects (dimension name obj_group). The mapping
    between objId and objGroup is provided by the ObjectGroupMap,
    which we determine when we define a combination, and which is
    written (to PFS/objectGroups) for later use. This allows us to
    select only the objId in the objGroup of interest when we coadd.

--- bin.src/defineCombination.py
+++ bin.src/defineCombination.py
@@ -7,19 +7,30 @@ from pfs.drp.stella.gen3 import defineCombination
 
 def main():
     """Command-line front-end to defineCombination"""
-    parser = ArgumentParser(description="Define a combination of exposures")
+    parser = ArgumentParser(description="Define a combination of visits")
     parser.add_argument("repo", help="URI for datastore repository")
     parser.add_argument("instrument", help="Instrument name or fully-qualified instrument class")
     parser.add_argument("name", help="Name of the combination")
-    parser.add_argument("--where", "-d", default=None, help="WHERE clause for selecting exposures")
+    parser.add_argument("--collections", default="PFS/raw/pfsConfig", help="Collections with pfsConfig")
+    parser.add_argument("--where", "-d", default=None, help="WHERE clause for selecting visits")
+    parser.add_argument(
+        "--max-group-size", type=int, default=2000, help="Maximum number of objects in a group"
+    )
     parser.add_argument(
         "--update", default=False, action="store_true", help="Update an existing registry"
     )
-    parser.add_argument("exposures", nargs="*", type=int, help="Exposure IDs to select")
+    parser.add_argument("visits", nargs="*", type=int, help="Visit IDs to select")
 
     args = parser.parse_args()
     return defineCombination(
-        args.repo, args.instrument, args.name, args.where, args.exposures, update=args.update
+        args.repo,
+        args.instrument,
+        args.name,
+        collections=args.collections,
+        where=args.where,
+        visitList=args.visits,
+        maxGroupSize=args.max_group_size,
+        update=args.update,
     )
 
 
