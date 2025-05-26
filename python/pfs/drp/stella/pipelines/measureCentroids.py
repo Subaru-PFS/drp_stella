82ce2be2b300eb62655c1d853bf29dbd7b643dd9
Paul Price <price@astro.princeton.edu>
2025-05-26 01:34:47 -1000

    cosmicray: produce crMask instead of calexp
    We want the calexp to include the cosmic ray mask as well as
    the scattered light correction, so writing it needs to be
    postponed until later. Instead, write a cosmic-ray mask that
    downstream tasks can apply.

--- python/pfs/drp/stella/pipelines/measureCentroids.py
+++ python/pfs/drp/stella/pipelines/measureCentroids.py
@@ -1,6 +1,6 @@
 import lsstDebug
 from lsst.afw.display import Display
-from lsst.afw.image import ExposureF
+from lsst.afw.image import ExposureF, Mask
 from lsst.pex.config import ConfigurableField, Field
 from lsst.pipe.base import PipelineTask, PipelineTaskConfig, PipelineTaskConnections, Struct
 from lsst.pipe.base import QuantumContext
@@ -26,9 +26,15 @@ class MeasureCentroidsConnections(
     """Connections for MeasureCentroidsTask"""
 
     exposure = InputConnection(
-        name="calexp",
+        name="postISRCCD",
         doc="Input ISR-corrected exposure",
         storageClass="Exposure",
+        dimensions=("instrument", "visit", "arm", "spectrograph"),
+    )
+    crMask = InputConnection(
+        name="crMask",
+        doc="Cosmic-ray mask",
+        storageClass="Mask",
         dimensions=("instrument", "visit", "arm", "spectrograph"),
     )
     pfsConfig = PrerequisiteConnection(
@@ -53,10 +59,19 @@ class MeasureCentroidsConnections(
         dimensions=("instrument", "visit", "arm", "spectrograph"),
     )
 
+    def __init__(self, *, config=None):
+        super().__init__(config=config)
+
+        if not config:
+            return
+        if not self.config.doApplyCrMask:
+            self.prerequisiteInputs.remove("crMask")
+
 
 class MeasureCentroidsConfig(PipelineTaskConfig, pipelineConnections=MeasureCentroidsConnections):
     """Configuration for MeasureCentroidsTask"""
 
+    doApplyCrMask = Field(dtype=bool, default=True, doc="Apply cosmic-ray mask to input exposure?")
     readLineList = ConfigurableField(
         target=ReadLineListTask, doc="Read line lists for detectorMap adjustment"
     )
@@ -94,7 +109,13 @@ class MeasureCentroidsTask(PipelineTask):
         butler.put(outputs, outputRefs)
         return outputs
 
-    def run(self, exposure: ExposureF, pfsConfig: PfsConfig, detectorMap: DetectorMap):
+    def run(
+        self,
+        exposure: ExposureF,
+        pfsConfig: PfsConfig,
+        detectorMap: DetectorMap,
+        crMask: Mask | None = None,
+    ):
         """Measure (both line and trace) centroids on an exposure
 
         Parameters
@@ -105,6 +126,8 @@ class MeasureCentroidsTask(PipelineTask):
             PFS fiber configuration.
         detectorMap : `DetectorMap`
             Mapping of fiberId,wavelength to x,y.
+        crMask : `Mask`, optional
+            Cosmic-ray mask.
 
         Returns
         -------
@@ -113,12 +136,16 @@ class MeasureCentroidsTask(PipelineTask):
         centroids : `ArcLineSet`
             Measured centroids.
         """
+        if self.config.doApplyCrMask:
+            if not crMask:
+                raise ValueError("Cosmic-ray mask required but not provided")
+            exposure.mask |= crMask
         refLines = self.readLineList.run(detectorMap, exposure.getMetadata())
         lines = self.centroidLines.run(exposure, refLines, detectorMap, pfsConfig, seed=exposure.visitInfo.id)
         if self.config.doForceTraces or not lines:
             traces = self.centroidTraces.run(exposure, detectorMap, pfsConfig)
             lines.extend(tracesToLines(detectorMap, traces, self.config.traceSpectralError))
-        return Struct(refLines=refLines, centroids=lines)
+        return Struct(exposure=exposure, refLines=refLines, centroids=lines)
 
 
 class MeasureDetectorMapConnections(MeasureCentroidsConnections):
@@ -168,7 +195,14 @@ class MeasureDetectorMapTask(MeasureCentroidsTask):
         butler.put(outputs.detectorMap, outputRefs.outputDetectorMap)
         return outputs
 
-    def run(self, exposure: ExposureF, pfsConfig: PfsConfig, detectorMap: DetectorMap, arm: str):
+    def run(
+        self,
+        exposure: ExposureF,
+        pfsConfig: PfsConfig,
+        detectorMap: DetectorMap,
+        arm: str,
+        crMask: Mask | None = None,
+    ):
         """Measure centroids on a single exposure and adjust the detectorMap
 
         Parameters
@@ -181,6 +215,8 @@ class MeasureDetectorMapTask(MeasureCentroidsTask):
             Mapping of fiberId,wavelength to x,y.
         arm : `str`
             Spectrograph arm in use (``b``, ``r``, ``n``, ``m``).
+        crMask : `Mask`, optional
+            Cosmic-ray mask.
 
         Returns
         -------
@@ -191,7 +227,7 @@ class MeasureDetectorMapTask(MeasureCentroidsTask):
         detectorMap : `DetectorMap`
             Adjusted mapping of fiberId,wavelength to x,y.
         """
-        data = super().run(exposure, pfsConfig, detectorMap)
+        data = super().run(exposure, pfsConfig, detectorMap, crMask=crMask)
 
         if self.debugInfo.detectorMap:
             display = Display(frame=1)
