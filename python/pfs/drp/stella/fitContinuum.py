733a05a0a124462f4f31b9017834f6f3662aa1c9
Paul Price <price@astro.princeton.edu>
2025-07-15 12:44:16 -0400

    Spectrum: add conversion from PfsSimpleSpectrum

--- python/pfs/drp/stella/fitContinuum.py
+++ python/pfs/drp/stella/fitContinuum.py
@@ -111,7 +111,9 @@ class FitContinuumTask(Task):
         continuum : `numpy.ndarray`
             Array of continuum fit.
         """
-        flux = spectrum.normFlux
+        flux = spectrum.flux
+        if hasattr(flux, "norm"):
+            flux /= flux.norm  # Normalize the flux to 1.0
         good = np.isfinite(flux)
         if self.config.doMaskLines and lines and spectrum.isWavelengthSet():
             good &= ~maskLines(spectrum.wavelength, lines.wavelength, self.config.maskLineRadius)
