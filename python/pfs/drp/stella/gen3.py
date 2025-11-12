7902fd3786446862eb8a462899fc5bee9b026e71
Sogo Mineo <31459905+SogoMineo@users.noreply.github.com>
2025-11-12 10:38:56 +0900

    Add ApplyPredefinedFluxCalTask
    ApplyPredefinedFluxCalTask is a subclass of ApplyFluxCalTask.
    This task uses predefined fluxCal (`fluxCal_predefined`)
    instead of a usual fluxCal. A predefined fluxCal is different from
    a usual fluxCal in that a dataId of the former does not have
    "visit" dimension, so that it will be applied to every exposure.

--- python/pfs/drp/stella/gen3.py
+++ python/pfs/drp/stella/gen3.py
@@ -1038,6 +1038,55 @@ def defineCombination(
     return visitHash, visitList
 
 
+def ingestPredefinedFluxCal(
+    repo: str,
+    instrument: str,
+    run: str,
+    path: str,
+    transfer: Optional[str] = "auto",
+) -> None:
+    """Ingest a predefined fluxCal into the datastore.
+
+    Parameters
+    ----------
+    repo : `str`
+        URI string of the Butler repo to use.
+    instrument : `str`
+        Instrument name or fully-qualified class name as a string.
+    run : `str`
+        The run in which the files should be ingested.
+    path : `str`
+        Path to the fluxCal FITS file to ingest.
+    transfer : `str`, optional
+        Transfer mode to use for ingest. If not `None`, must be one of 'auto',
+        'move', 'copy', 'direct', 'split', 'hardlink', 'relsymlink' or
+        'symlink'.
+    """
+    log = getLogger("pfs.ingestPredefinedFluxCal")
+
+    if not os.path.isfile(path):
+        raise RuntimeError(f"Not a file: '{path}'")
+
+    butler = Butler.from_config(repo, run=run)
+
+    datasetType = DatasetType(
+        name="fluxCal_predefined",
+        dimensions=DimensionGroup(universe=butler.registry.dimensions, names=["instrument"]),
+        storageClass="FocalPlaneFunction",
+    )
+    dataId = {"instrument": Instrument.from_string(instrument, butler.registry).getName()}
+
+    dataset = FileDataset(
+        path=ResourcePath(path, root=os.getcwd(), forceAbsolute=True),
+        refs=[DatasetRef(datasetType, dataId, run)],
+    )
+
+    log.info("Ingesting %s...", path)
+
+    butler.registry.registerDatasetType(datasetType)
+    butler.ingest(dataset, transfer=transfer)
+
+
 def cleanRun(
     repo: str,
     collections: str,
