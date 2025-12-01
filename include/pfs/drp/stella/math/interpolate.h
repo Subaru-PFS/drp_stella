7bfc7110783b0434fb895371bf70c7a3bf222958
Paul Price <price@astro.princeton.edu>
2025-12-01 17:54:02 -0500

    implement linear interpolation in C++
    We've been implementing it in python (as it was before we
    implemented the Lanczos interpolation), but that made it
    a completely separate code path. Implementing it with the
    same code as for the Lanczos interpolation may help us
    find any little errors or problems, and fix them for both.

--- include/pfs/drp/stella/math/interpolate.h
+++ include/pfs/drp/stella/math/interpolate.h
@@ -1,5 +1,5 @@
-#ifndef PFS_DRP_STELLA_LANCZOS_H
-#define PFS_DRP_STELLA_LANCZOS_H
+#ifndef PFS_DRP_STELLA_INTERPOLATE_H
+#define PFS_DRP_STELLA_INTERPOLATE_H
 
 #include "ndarray_fwd.h"
 
@@ -21,20 +21,59 @@ float const INTERPOLATE_DEFAULT_MIN_WEIGHT = 0.3;
 namespace impl {
 
 
+/// Triangle function, for linear interpolation kernel
+///
+/// This is a simple triangular function that goes to zero at +/- 1.
+class TriangleFunction1 : public lsst::afw::math::Function1<double> {
+public:
+    TriangleFunction1() : lsst::afw::math::Function1<double>(0) {}
+    ~TriangleFunction1() noexcept override = default;
+
+    std::shared_ptr<lsst::afw::math::Function1<double>> clone() const override {
+        return std::make_shared<TriangleFunction1>(*this);
+    }
+
+    double operator()(double x) const override {
+        double const xAbs = std::abs(x);
+        if (xAbs >= 1.0) {
+            return 0.0;
+        }
+        return 1.0 - xAbs;
+    }
+
+    std::string toString(std::string const& prefix) const override { return "TriangleFunction1()"; }
+};
+
+
+template <typename KernelT>
+int getKernelHalfSize(KernelT const& kernel);
+
+template <>
+int getKernelHalfSize(TriangleFunction1 const& kernel) {
+    return 1;
+}
+
+template <>
+int getKernelHalfSize(lsst::afw::math::LanczosFunction1<double> const& kernel) {
+    return kernel.getOrder();
+}
+
+
 //@{
-/// Lanczos interpolation of 1D spectrum
+/// Interpolation of 1D spectrum
 ///
 /// This is a general implementation, handling flux, mask and variance, and used
-/// by lanczosInterpolateFlux() and lanczosInterpolateVariance(). Template
-/// parameters control which of the input and output arrays are provided and
-/// used, allowing the compiler to optimize away unused branches. If an array
-/// is not used according to the template parameters, any input array
-/// will be ignored; using an empty array is recommended in this case.
+/// by interpolateFlux() and interpolateVariance(). Template parameters control
+/// which of the input and output arrays are provided and used, allowing the
+/// compiler to optimize away unused branches. If an array is not used according
+/// to the template parameters, any input array will be ignored; using an empty
+/// array is recommended in this case.
 ///
 /// @tparam wantValues  True if we want to compute resultValues.
 /// @tparam wantMask  True if we want to compute resultMask.
 /// @tparam wantVariance  True if we want to compute resultVariance.
 /// @tparam haveMask  True if inputMask is provided.
+/// @tparam KernelT  The kernel function type.
 /// @tparam T  The values and variance array element type.
 /// @tparam U  The indices array element type.
 /// @tparam C1  The resultValues array's contiguity.
@@ -62,7 +101,7 @@ namespace impl {
 /// @param[in] indices  The indices array, with floating-point indices at which
 ///    to interpolate.
 /// @param[in] fill  The value to use when interpolation is not possible.
-/// @param[in] order  The order of the Lanczos function.
+/// @param[in] order  The order of the interpolation kernel.
 /// @param[in] minWeight  The minimum sum of weights to accept; if the sum of
 ///    weights is less than this value, the output will be masked.
 template <
@@ -70,11 +109,13 @@ template <
     bool wantMask,  // true means have resultMask
     bool wantVariance,  // true means have resultVariance and inputVariance
     bool haveMask,  // true means have inputMask
+    typename KernelT,
     typename T,
     typename U,
     int C1, int C2, int C3, int C4, int C5, int C6, int C7
 >
-void lanczosInterpolate(
+void interpolate(
+    KernelT const& kernel,
     ndarray::Array<T, 1, C1> & resultValues,
     ndarray::Array<bool, 1, C2> & resultMask,
     ndarray::Array<T, 1, C3> & resultVariance,
@@ -83,7 +124,6 @@ void lanczosInterpolate(
     ndarray::Array<T, 1, C6> const& inputVariance,
     ndarray::Array<U, 1, C7> const& indices,
     T fill,
-    unsigned int order,
     double minWeight=INTERPOLATE_DEFAULT_MIN_WEIGHT
 ) {
     static_assert(
@@ -124,7 +164,7 @@ void lanczosInterpolate(
         );
     }
 
-    lsst::afw::math::LanczosFunction1<double> const lanczos{order};
+    int const halfSize = getKernelHalfSize(kernel);
     for (std::size_t ii = 0; ii != numOut; ++ii) {
         T const target = indices[ii];
         if (target < 0 || target > numIn - 1) {
@@ -145,25 +185,25 @@ void lanczosInterpolate(
         // |-0-|-1-|-2-|-3-|-4-|-5-|-6-|-7-|
         //              ^ target = 2.75
         //               ^ index = 3
-        // Want the center of the Lanczos window to be between index-1 and index
+        // Want the center of the kernel window to be between index-1 and index
         // frac = 0.75
-        // For order=3:
+        // For order=3 --> halfSize=3:
         // start = 0
         // end = 5
         // dx = -2.75, -1.75, -0.75, 0.25, 1.25, 2.25
 
-        // Interpolate using a window of size 2*order centered on target
+        // Interpolate using a window of size 2*halfSize centered on target
         double value = 0.0;
         double variance = 0.0;
         double sumWeights = 0.0;
-        std::ptrdiff_t const start = std::max(std::ptrdiff_t(0), index - order);  // inclusive
-        std::ptrdiff_t const end = std::min(std::ptrdiff_t(numIn), index + order);  // exclusive
+        std::ptrdiff_t const start = std::max(std::ptrdiff_t(0), index - halfSize);  // inclusive
+        std::ptrdiff_t const end = std::min(std::ptrdiff_t(numIn), index + halfSize);  // exclusive
         double frac = double(start) - target;
         for (std::ptrdiff_t jj = start; jj < end; ++jj, frac += 1.0) {
             if (haveMask && inputMask[jj]) {
                 continue;
             }
-            double weight = lanczos(frac);
+            double const weight = kernel(frac);
             sumWeights += weight;
             if (wantValues) {
                 value += weight * inputValues[jj];
@@ -192,16 +232,56 @@ void lanczosInterpolate(
         }
     }
 }
+template <
+    bool wantValues,  // true means have resultValues and inputValues
+    bool wantMask,  // true means have resultMask
+    bool wantVariance,  // true means have resultVariance and inputVariance
+    bool haveMask,  // true means have inputMask
+    typename T,
+    typename U,
+    int C1, int C2, int C3, int C4, int C5, int C6, int C7
+>
+void interpolate(
+    ndarray::Array<T, 1, C1> & resultValues,
+    ndarray::Array<bool, 1, C2> & resultMask,
+    ndarray::Array<T, 1, C3> & resultVariance,
+    ndarray::Array<T, 1, C4> const& inputValues,
+    ndarray::Array<bool, 1, C5> const& inputMask,
+    ndarray::Array<T, 1, C6> const& inputVariance,
+    ndarray::Array<U, 1, C7> const& indices,
+    T fill,
+    int order,
+    double minWeight=INTERPOLATE_DEFAULT_MIN_WEIGHT
+) {
+    if (order <= 0) {
+        throw LSST_EXCEPT(lsst::pex::exceptions::InvalidParameterError, "Order must be positive");
+    }
+    if (order == 1) {
+        interpolate<wantValues, wantMask, wantVariance, haveMask>(
+            impl::TriangleFunction1(),
+            resultValues, resultMask, resultVariance,
+            inputValues, inputMask, inputVariance,
+            indices, fill, minWeight
+        );
+    } else {
+        interpolate<wantValues, wantMask, wantVariance, haveMask>(
+            lsst::afw::math::LanczosFunction1<double>(order),
+            resultValues, resultMask, resultVariance,
+            inputValues, inputMask, inputVariance,
+            indices, fill, minWeight
+        );
+    }
+}
 //@}
 
 
 }  // namespace impl
 
 
-/// Lanczos interpolation of spectrum values, mask and variance
+/// Interpolation of spectrum values, mask and variance
 ///
 /// This is a full-featured interpolation function, wrapping the general
-/// implementation in impl::lanczosInterpolate().
+/// implementation in impl::interpolate().
 ///
 /// @tparam T  The values and variance array element type.
 /// @tparam U  The indices array element type.
@@ -224,11 +304,11 @@ void lanczosInterpolate(
 /// @param[in] indices  The indices array, with floating-point indices at which
 ///    to interpolate.
 /// @param[in] fill  The value to use when interpolation is not possible.
-/// @param[in] order  The order of the Lanczos function.
+/// @param[in] order  The order of the interpolation (1 = Triangle, >1 = Lanczos of given order).
 /// @param[in] minWeight  The minimum sum of weights to accept; if the sum of
 ///    weights is less than this value, the output will be masked.
 template <typename T, typename U, int C1, int C2, int C3, int C4, int C5, int C6, int C7>
-void lanczosInterpolate(
+void interpolate(
     ndarray::Array<T, 1, C1> & resultValues,
     ndarray::Array<bool, 1, C2> & resultMask,
     ndarray::Array<T, 1, C3> & resultVariance,
@@ -240,7 +320,7 @@ void lanczosInterpolate(
     unsigned int order=INTERPOLATE_DEFAULT_ORDER,
     double minWeight=INTERPOLATE_DEFAULT_MIN_WEIGHT
 ) {
-    impl::lanczosInterpolate<true, true, true, true>(
+    return impl::interpolate<true, true, true, true>(
         resultValues, resultMask, resultVariance,
         inputValues, inputMask, inputVariance,
         indices, fill, order, minWeight
@@ -249,7 +329,7 @@ void lanczosInterpolate(
 
 
 //@{
-/// Lanczos interpolation of 1D flux spectrum
+/// Interpolation of 1D flux spectrum
 ///
 /// For use when variance is not needed.
 ///
@@ -269,11 +349,11 @@ void lanczosInterpolate(
 /// @param[in] indices  The indices array, with floating-point indices at which
 ///    to interpolate.
 /// @param[in] fill  The value to use when interpolation is not possible.
-/// @param[in] order  The order of the Lanczos function.
+/// @param[in] order  Interpolation order (1 = Triangle, >1 = Lanczos of given order).
 /// @param[in] minWeight  The minimum sum of weights to accept; if the sum of
 ///    weights is less than this value, the output will be masked.
 template <typename T, typename U, int C1, int C2, int C3, int C4, int C5>
-void lanczosInterpolateFlux(
+void interpolateFlux(
     ndarray::Array<T, 1, C1> & resultValues,
     ndarray::Array<bool, 1, C2> & resultMask,
     ndarray::Array<T, 1, C3> const& inputValues,
@@ -284,12 +364,12 @@ void lanczosInterpolateFlux(
     double minWeight=INTERPOLATE_DEFAULT_MIN_WEIGHT
 ) {
     ndarray::Array<T, 1, 1> emptyVar;  // Empty array for variance
-    impl::lanczosInterpolate<true, true, false, true>(
+    impl::interpolate<true, true, false, true>(
         resultValues, resultMask, emptyVar, inputValues, inputMask, emptyVar, indices, fill, order, minWeight
     );
 }
 template <typename T, typename U, int C1, int C3, int C4, int C5>
-ndarray::Array<T, 1, C1> & lanczosInterpolateFlux(
+ndarray::Array<T, 1, C1> & interpolateFlux(
     ndarray::Array<T, 1, C1> & resultValues,
     ndarray::Array<T, 1, C3> const& inputValues,
     ndarray::Array<bool, 1, C4> const& inputMask,
@@ -299,13 +379,13 @@ ndarray::Array<T, 1, C1> & lanczosInterpolateFlux(
 ) {
     ndarray::Array<bool, 1, 1> emptyMask;  // Empty array for mask
     ndarray::Array<T, 1, 1> emptyVar;  // Empty array for variance
-    impl::lanczosInterpolate<true, false, false, true>(
+    impl::interpolate<true, false, false, true>(
         resultValues, emptyMask, emptyVar, inputValues, inputMask, emptyVar, indices, fill, order
     );
     return resultValues;
 }
 template <typename T, typename U, int C3, int C4, int C5>
-ndarray::Array<T, 1, 1> lanczosInterpolateFlux(
+ndarray::Array<T, 1, 1> interpolateFlux(
     ndarray::Array<T, 1, C3> const& inputValues,
     ndarray::Array<bool, 1, C4> const& inputMask,
     ndarray::Array<U, 1, C5> const& indices,
@@ -313,10 +393,10 @@ ndarray::Array<T, 1, 1> lanczosInterpolateFlux(
     unsigned int order=INTERPOLATE_DEFAULT_ORDER
 ) {
     ndarray::Array<T, 1, 1> result = ndarray::allocate(indices.size());
-    return lanczosInterpolateFlux(result, inputValues, inputMask, indices, fill, order);
+    return interpolateFlux(result, inputValues, inputMask, indices, fill, order);
 }
 template <typename T, typename U, int C3, int C5>
-ndarray::Array<T, 1, 1> lanczosInterpolateFlux(
+ndarray::Array<T, 1, 1> interpolateFlux(
     ndarray::Array<T, 1, C3> const& inputValues,
     ndarray::Array<U, 1, C5> const& indices,
     T fill=INTERPOLATE_DEFAULT_FILL,
@@ -325,7 +405,7 @@ ndarray::Array<T, 1, 1> lanczosInterpolateFlux(
     ndarray::Array<T, 1, 1> result = ndarray::allocate(indices.size());
     ndarray::Array<bool, 1, 1> emptyMask;  // Empty array for mask
     ndarray::Array<T, 1, 1> emptyVar;  // Empty array for variance
-    impl::lanczosInterpolate<true, false, false, false>(
+    impl::interpolate<true, false, false, false>(
         result, emptyMask, emptyVar, inputValues, emptyMask, emptyVar, indices, fill, order
     );
     return result;
@@ -334,7 +414,7 @@ ndarray::Array<T, 1, 1> lanczosInterpolateFlux(
 
 
 //@{
-/// Lanczos interpolation of 1D variance spectrum
+/// Interpolation of 1D variance spectrum
 ///
 /// For use when flux is not needed.
 ///
@@ -355,11 +435,11 @@ ndarray::Array<T, 1, 1> lanczosInterpolateFlux(
 /// @param[in] indices  The indices array, with floating-point indices at which
 ///    to interpolate.
 /// @param[in] fill  The variance to use when interpolation is not possible.
-/// @param[in] order  The order of the Lanczos function.
+/// @param[in] order  Interpolation order (1 = Triangle, >1 = Lanczos of given order).
 /// @param[in] minWeight  The minimum sum of weights to accept; if the sum of
 ///    weights is less than this value, the output will be masked.
 template <typename T, typename U, int C1, int C2, int C3, int C4, int C5>
-void lanczosInterpolateVariance(
+void interpolateVariance(
     ndarray::Array<T, 1, C1> & resultVariance,
     ndarray::Array<bool, 1, C2> & resultMask,
     ndarray::Array<T, 1, C3> const& inputVariance,
@@ -370,12 +450,12 @@ void lanczosInterpolateVariance(
     double minWeight=INTERPOLATE_DEFAULT_MIN_WEIGHT
 ) {
     ndarray::Array<T, 1, 1> empty;  // Empty array for values
-    impl::lanczosInterpolate<false, true, true, true>(
+    impl::interpolate<false, true, true, true>(
         empty, resultMask, resultVariance, empty, inputMask, inputVariance, indices, fill, order, minWeight
     );
 }
 template <typename T, typename U, int C1, int C3, int C4, int C5>
-ndarray::Array<T, 1, C1> & lanczosInterpolateVariance(
+ndarray::Array<T, 1, C1> & interpolateVariance(
     ndarray::Array<T, 1, C1> & resultVariance,
     ndarray::Array<T, 1, C3> const& inputVariance,
     ndarray::Array<bool, 1, C4> const& inputMask,
@@ -385,13 +465,13 @@ ndarray::Array<T, 1, C1> & lanczosInterpolateVariance(
 ) {
     ndarray::Array<T, 1, 1> emptyVal;  // Empty array for variance
     ndarray::Array<bool, 1, 1> emptyMask;  // Empty array for mask
-    impl::lanczosInterpolate<false, false, true, true>(
+    impl::interpolate<false, false, true, true>(
         emptyVal, emptyMask, resultVariance, emptyVal, inputMask, inputVariance, indices, fill, order
     );
     return resultVariance;
 }
 template <typename T, typename U, int C3, int C4, int C5>
-ndarray::Array<T, 1, 1> lanczosInterpolateVariance(
+ndarray::Array<T, 1, 1> interpolateVariance(
     ndarray::Array<T, 1, C3> const& inputVariance,
     ndarray::Array<bool, 1, C4> const& inputMask,
     ndarray::Array<U, 1, C5> const& indices,
@@ -399,10 +479,10 @@ ndarray::Array<T, 1, 1> lanczosInterpolateVariance(
     unsigned int order=INTERPOLATE_DEFAULT_ORDER
 ) {
     ndarray::Array<T, 1, 1> result = ndarray::allocate(indices.size());
-    return lanczosInterpolateVariance(result, inputVariance, inputMask, indices, fill, order);
+    return interpolateVariance(result, inputVariance, inputMask, indices, fill, order);
 }
 template <typename T, typename U, int C3, int C5>
-ndarray::Array<T, 1, 1> lanczosInterpolateVariance(
+ndarray::Array<T, 1, 1> interpolateVariance(
     ndarray::Array<T, 1, C3> const& inputVariance,
     ndarray::Array<U, 1, C5> const& indices,
     T fill=INTERPOLATE_DEFAULT_FILL,
@@ -411,7 +491,7 @@ ndarray::Array<T, 1, 1> lanczosInterpolateVariance(
     ndarray::Array<T, 1, 1> result = ndarray::allocate(indices.size());
     ndarray::Array<bool, 1, 1> emptyMask;  // Empty array for mask
     ndarray::Array<T, 1, 1> emptyVal;  // Empty array for variance
-    impl::lanczosInterpolate<false, false, true, false>(
+    impl::interpolate<false, false, true, false>(
         emptyVal, emptyMask, result, emptyVal, emptyMask, inputVariance, indices, fill, order
     );
     return result;
