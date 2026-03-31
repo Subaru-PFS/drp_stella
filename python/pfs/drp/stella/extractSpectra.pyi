c97704a02538f2871385f8b1e3810261f48e9a78
Paul Price <price@astro.princeton.edu>
2026-03-31 11:59:07 -0400

    add warping

--- python/pfs/drp/stella/extractSpectra.pyi
+++ python/pfs/drp/stella/extractSpectra.pyi
@@ -5,7 +5,10 @@ def extractSpectra(
     image: MaskedImage,
     fiberTraces: FiberTraceSet,
     badBitMask: int = 0,
-    bgBlockSize: int = 100,
+    kernelHalfWidth: int = 2,
+    kernelOrder: int = 3,
+    xBackgroundSize: int = 100,
+    yBackgroundSize: int = 100,
     minFracMask: float = 0.3,
     minFracImage: float = 0.4,
 ) -> tuple[SpectrumSet, Image]: ...
