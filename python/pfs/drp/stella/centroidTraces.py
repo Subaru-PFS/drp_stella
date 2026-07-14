81a7e77df5271908d4b5f8c2108c1860c622ca65
Paul Price <price@astro.princeton.edu>
2026-07-14 17:40:19 -0400

    centroidTraces: expand searchRadius
    The spectrographs (especially SM1) can move a lot more
    than we thought. I think a searchRadius=3 is about the
    largest we can tolerate without algorithmic changes,
    since it's half the distance between fibers.

--- python/pfs/drp/stella/centroidTraces.py
+++ python/pfs/drp/stella/centroidTraces.py
@@ -26,7 +26,7 @@ class CentroidTracesConfig(Config):
     kernelSize = Field(dtype=float, default=4.0, doc="Size of convolution kernel (sigma)")
     mask = ListField(dtype=str, default=["CR", "BAD", "NO_DATA"], doc="Mask planes to ignore")
     threshold = Field(dtype=float, default=10.0, doc="Signal-to-noise threshold for trace")
-    searchRadius = Field(dtype=float, default=1, doc="Radius about the expected peak to search")
+    searchRadius = Field(dtype=float, default=3.0, doc="Radius about the expected peak to search")
 
 
 class CentroidTracesTask(Task):
